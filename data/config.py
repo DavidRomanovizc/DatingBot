772cca52b7e9550a281965142d8bfb69e3381fad
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-09-09 19:23:12 +0300

    added Data

---
+++ data/config.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from environs import Env
+
+# Теперь используем вместо библиотеки python-dotenv библиотеку environs
+env = Env()
+env.read_env()
+
+BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
+ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
+IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+
+DB_USER = env.str('DB_USER')
+DB_PASS = env.str('DB_PASS')
+DB_HOST = env.str('DB_HOST')
+DB_NAME = env.str('DB_NAME')
+
+I18N_DOMAIN = "testbot"
+BASE_DIR = Path(__file__).parent
+LOCALES_DIR = BASE_DIR / "locales"  # тут хранятся переводы
+
+
+# Новые переменные для библиотеки QiwiApi
+QIWI_TOKEN = env.str("qiwi")
+WALLET_QIWI = env.str("wallet")  # номер телефона
+QIWI_PUBKEY = env.str("qiwi_p_pub")  # Публичный ключ для генерации ссылки на оплаты
---
+++ data/subs.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Item:
+    id: int
+    title: str
+    description: str
+    price: int
+    photo_link: str
+
+
+Sub_lvl_1 = Item(
+    id=1,
+    title="Поддержка нашего проекта",
+    description="""
+    Поддержка проекта
+    """,
+    price=50,
+    photo_link="https://sun9-50.userapi.com/impg/t9W9XA1VCMhgq9hxDDI5kpNT9Rma1LUlfuOl2Q/P6J1lsYChTc.jpg?size=2118x1089&quality=96&sign=fbbd37bc2a2e7731e72d16845fdea93f&type=album"
+)
+
+items = [Sub_lvl_1]
