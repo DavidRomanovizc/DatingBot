77f7e41b3a8e2742ea8e4da427971b0e8a35d727
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-09-30 17:49:50 +0300

    Update

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import logging
-import os
-
-import django
 from aiogram import executor
 
 from loader import dp, db, bot, storage
@@ -13,33 +10,17 @@ from utils.set_bot_commands import set_default_commands
 async def on_startup(dp):
     import filters
     import middlewares
-    # filters.setup(dp)
     middlewares.setup(dp)
 
-    logging.info(f'Подключаюсь к базе данных...')
     await db.create()
 
-    # logging.info(f'Создаю таблицу пользователей...')
-    # await db.create_table_users()
-    #
-    # logging.info(f'Создаю таблицу платежей...')
-    # await db.create_table_payments()
-
-    logging.info(f'Готово!')
-
     from utils.notify_admins import on_startup_notify
-    # try:
-    #     db.create_table_users()
-    # except Exception as e:
-    #     print(e)
-    # db.delete_users()
-    # print(db.select_all_users())
 
     await on_startup_notify(dp)
     await set_default_commands(dp)
 
 
-async def on_shutdown(dp):
+async def on_shutdown():
     await bot.close()
     await storage.close()
 
