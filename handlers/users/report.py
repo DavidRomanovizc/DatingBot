814b94eb8d1c729536dd9962173180a5f001b793
valisov <kldgrcorp@gmail.com>
2021-09-30 21:52:16 +0300

    easy refactor and optimization + fix

--- handlers/users/report.py
+++ handlers/users/report.py
@@ -1,3 +1,6 @@
+import random
+
+from handlers.users.view import select_all_users_list, create_questionnaire
 from keyboards.inline.BN_report import report_inline_kb
 from aiogram.dispatcher import FSMContext
 from aiogram.types import CallbackQuery
@@ -24,24 +27,6 @@ async def report_user(call: CallbackQuery):
 
 @dp.callback_query_handler(text="content", state=Report.R1)
 @dp.callback_query_handler(text="drugs", state=Report.R1)
-async def report_user(call: CallbackQuery, state: FSMContext):
-    await call.answer(cache_time=60)
-    display_name = call.from_user.full_name
-
-    await call.answer(
-        f"Репорт на пользователя успешно отправлен.\n"
-        "Администрация предпримет все необходимые меры"
-    )
-
-    for admin_id in ADMINS:
-        await bot.send_message(
-            admin_id,
-            f"Кинут репорт на пользователя "
-        )
-    async with state.proxy() as data:
-        data["report_us"] = display_name
-
-
 @dp.callback_query_handler(text="scam", state=Report.R1)
 @dp.callback_query_handler(text="another", state=Report.R1)
 async def report_user(call: CallbackQuery, state: FSMContext):
@@ -62,10 +47,10 @@ async def report_user(call: CallbackQuery, state: FSMContext):
         data["report_us"] = display_name
 
 
-# FIXME: Почините
-@dp.callback_query_handler(text="cancel_3")
-async def cancel_3(call: CallbackQuery):
-    await call.answer(cache_time=60)
-    await call.message.answer(f"Отмена")
-    await call.message.edit_reply_markup(reply_markup=None)
-# FIXME: Почините
+@dp.callback_query_handler(text="cancel_report", state=Report.R1)
+async def cancel_report(call: CallbackQuery, state: FSMContext):
+    await call.message.delete()
+    user_list = await select_all_users_list()
+    random_user = random.choice(user_list)
+    await create_questionnaire(random_user=random_user, chat_id=call.from_user.id)
+    await state.reset_state()
