5caa5557adc7ba305498bb3740af2c9ccaaf082a
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-02 23:20:58 +0300

    improved throttling system

--- middlewares/throttling.py
+++ middlewares/throttling.py
@@ -1,10 +1,9 @@
-import asyncio
-
-from aiogram import types, Dispatcher
+from aiogram import Dispatcher, types
 from aiogram.dispatcher import DEFAULT_RATE_LIMIT
 from aiogram.dispatcher.handler import CancelHandler, current_handler
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.utils.exceptions import Throttled
+import asyncio
 
 
 class ThrottlingMiddleware(BaseMiddleware):
@@ -12,17 +11,18 @@ class ThrottlingMiddleware(BaseMiddleware):
     Simple middleware
     """
 
-    def __init__(self, limit=DEFAULT_RATE_LIMIT, key_prefix='antiflood_'):
+    def __init__(self, limit=DEFAULT_RATE_LIMIT, key_prefix="antiflood_"):
         self.rate_limit = limit
         self.prefix = key_prefix
         super(ThrottlingMiddleware, self).__init__()
 
+    # noinspection PyUnusedLocal
     async def on_process_message(self, message: types.Message, data: dict):
         handler = current_handler.get()
         dispatcher = Dispatcher.get_current()
         if handler:
-            limit = getattr(handler, "throttling_rate_limit", self.rate_limit)
-            key = getattr(handler, "throttling_key", f"{self.prefix}_{handler.__name__}")
+            limit = getattr(handler, 'throttling_rate_limit', self.rate_limit)
+            key = getattr(handler, 'throttling_key', f"{self.prefix}_{handler.__name__}")
         else:
             limit = self.rate_limit
             key = f"{self.prefix}_message"
@@ -33,5 +33,20 @@ class ThrottlingMiddleware(BaseMiddleware):
             raise CancelHandler()
 
     async def message_throttled(self, message: types.Message, throttled: Throttled):
+        handler = current_handler.get()
+        dispatcher = Dispatcher.get_current()
+        if handler:
+            key = getattr(handler, 'throttling_key', f"{self.prefix}_{handler.__name__}")
+        else:
+            key = f"{self.prefix}_message"
+        delta = throttled.rate - throttled.delta
         if throttled.exceeded_count <= 2:
-            await message.reply("Too many requests!")
+            service_message = await message.reply('Too many requests! ')
+
+            await asyncio.sleep(5)
+            await service_message.delete()
+            await message.delete()
+        try:
+            await message.delete()
+        except Exception as err:
+            pass
