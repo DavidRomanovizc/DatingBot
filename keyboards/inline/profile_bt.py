9fad9c059b6d2d53035a57bbb13c4fb501bc4acc
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-21 00:26:34 +0300

    ♻️ Refactor code

--- keyboards/inline/profile_bt.py
+++ keyboards/inline/profile_bt.py
@@ -3,9 +3,6 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 reg_profile = InlineKeyboardMarkup(row_width=3,
                                    inline_keyboard=[
                                        [
-                                           InlineKeyboardButton(text="Пройти опрос",
-                                                                callback_data="survey"),
-
+                                           InlineKeyboardButton(text="Пройти опрос", callback_data="survey")
                                        ],
-                                   ]
-                                   )
+                                   ])
