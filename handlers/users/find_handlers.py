dee2dfd190340a70eb9b221eb06bcd5ac0c0d8a5
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-21 00:31:51 +0300

    ♻️ Refactor code

--- handlers/users/find_handlers.py
+++ handlers/users/find_handlers.py
@@ -6,6 +6,6 @@ from loader import dp, bot
 @dp.callback_query_handler(text='start_finder')
 async def start_finder(callback_query: CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, f'здесь будут появлятся анкеты других '
+    await bot.send_message(callback_query.from_user.id, f'здесь будут появляться анкеты других '
                                                         f'людей с такими кнопками',
                            reply_markup=questionnaires_inline_kb)
