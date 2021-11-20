87bb6454d8723ed469d5658dbbad3d547d3d0a00
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-21 00:29:39 +0300

    ♻️ Refactor code

--- keyboards/inline/lifestyle_choice_inline.py
+++ keyboards/inline/lifestyle_choice_inline.py
@@ -1,23 +1,23 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 lifestyle_inline_kb = InlineKeyboardMarkup(row_width=3,
-                                      inline_keyboard=[
-                                          [
-                                              InlineKeyboardButton(text='Учусь',
-                                                                   callback_data='study_lifestyle'),
+                                           inline_keyboard=[
+                                               [
+                                                   InlineKeyboardButton(text='Учусь',
+                                                                        callback_data='study_lifestyle'),
 
-                                              InlineKeyboardButton(text='Работаю',
-                                                                   callback_data='work_lifestyle')
-                                          ],
+                                                   InlineKeyboardButton(text='Работаю',
+                                                                        callback_data='work_lifestyle')
+                                               ],
 
-                                          [
-                                              InlineKeyboardButton(text='Ищу работу',
-                                                                   callback_data='job_find_lifestyle')
-                                          ],
-                                          [
-                                              InlineKeyboardButton(text='Домохозяйка/Домохозяин',
-                                                                   callback_data='householder_lifestyle')
-                                          ],
+                                               [
+                                                   InlineKeyboardButton(text='Ищу работу',
+                                                                        callback_data='job_find_lifestyle')
+                                               ],
+                                               [
+                                                   InlineKeyboardButton(text='Домохозяйка/Домохозяин',
+                                                                        callback_data='householder_lifestyle')
+                                               ],
 
-                                      ]
-                                      )
+                                           ]
+                                           )
