168d056f85193a0e78db6e39e973d80ad426bd5f
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-21 00:25:41 +0300

    ♻️ Took out two handlers in new files

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,8 +1,10 @@
 from aiogram.dispatcher.filters.builtin import CommandStart
 from asyncpg import UniqueViolationError
 
-from keyboards.inline.inline_start_menu import inline_start
+from keyboards.inline.back_bnt import go_back_to_the_menu
+from keyboards.inline.main_menu import inline_start
 from aiogram.types import CallbackQuery
+
 from loader import dp, db, _
 from aiogram import types
 
@@ -33,28 +35,3 @@ async def change_language(call: CallbackQuery):
     await call.message.answer(_("Ваш язык был изменен", locale=lang))
 
 
-@dp.callback_query_handler(text_contains="info")
-async def information(call: CallbackQuery):
-    await call.answer(cache_time=60)
-    await call.message.edit_text("<b>Made by: </b>\n"
-                                 "\n"
-                                 "@DRomanovizc - Python Developer\n"
-                                 "@mroshalom - Python Developer\n"
-                                 "\n"
-                                 "ПОЛЬЗУЯСЬ БОТОМ ВЫ АВТОМАТИЧЕСКИ СОГЛАШАЕТЕСЬ НА ОБРАБОТКУ ПЕРСОНАЛЬНЫХ ДАННЫХ\n"
-                                 "<i>Dslango© 2021</i>",
-                                 reply_markup=inline_start)
-
-
-@dp.callback_query_handler(text_contains="instruction")
-async def get_inst(call: CallbackQuery):
-    await call.answer(cache_time=60)
-    await call.message.edit_text(f"<b>Инструкция: </b>\n\n"
-                                 f"<b>1. Навигация по анкетам\n\n</b>"
-                                 f"👍 - <i>вам понравилась анкета другого пользователя</i>\n"
-                                 f"👎 - <i>вам не понравилась анкета</i>\n"
-                                 f"💌 - <i>отправить через бота сообщение</i>\n"
-                                 f"🛑 - <i>пожаловаться на анкету/пользователя</i>\n\n"
-                                 f"Если вы нашли баг, то можете сообщить нам, написав сюда\n - @DRomanovizc или "
-                                 f"@mroshalom",
-                                 reply_markup=inline_start)
