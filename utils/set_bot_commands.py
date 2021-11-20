2cd3b068c3cf8cf778857c69705b2b74bdb6480b
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-21 00:28:14 +0300

    ♻️ Refactor code

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -3,6 +3,5 @@ from aiogram import types
 
 async def set_default_commands(dp):
     await dp.bot.set_my_commands([
-        types.BotCommand("start", "Запустить бота"),
-        types.BotCommand("help", "Помощь"),
+        types.BotCommand("start", "Запустить бота")
     ])
