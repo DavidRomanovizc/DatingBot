7d53b19054630ae7be13f86bd047cabd76861b3e
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-21 00:29:27 +0300

    ♻️ Refactor code

---
+++ keyboards/inline/main_menu.py
@@ -0,0 +1,22 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+inline_start = InlineKeyboardMarkup(row_width=2,
+                                    inline_keyboard=
+                                    [
+                                        [
+                                            InlineKeyboardButton(text="➕ Регистрация",
+                                                                 callback_data="registration"),
+                                            InlineKeyboardButton(text="📄 Меню бота",
+                                                                 callback_data="second_m")
+                                        ],
+                                        [
+                                            InlineKeyboardButton(text="📝 Информация", callback_data="info")
+                                        ],
+
+                                        [
+                                            InlineKeyboardButton(text="💚 Спонсорство",
+                                                                 url="https://www.donationalerts.com/r/david_romanov"),
+                                            InlineKeyboardButton(text="🌐 Инструкция", callback_data="instruction")
+                                        ]
+                                    ]
+                                    )
