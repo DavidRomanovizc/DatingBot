d597d5e791ba7bd473b86b8ee8ea4aeb4ac927ce
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-23 17:40:27 +0300

    Refactor code

--- handlers/errors/error_handler.py
+++ handlers/errors/error_handler.py
@@ -9,48 +9,40 @@ from loader import dp
 
 @dp.errors_handler()
 async def errors_handler(update, exception):
-    """
-    Exceptions handler. Catches all exceptions within task factory tasks.
-    :param dispatcher:
-    :param update:
-    :param exception:
-    :return: stdout logging
-    """
-
     if isinstance(exception, CantDemoteChatCreator):
         logging.exception("Can't demote chat creator")
         return True
 
-    if isinstance(exception, MessageNotModified):
+    elif isinstance(exception, MessageNotModified):
         logging.exception('Message is not modified')
         return True
-    if isinstance(exception, MessageCantBeDeleted):
+    elif isinstance(exception, MessageCantBeDeleted):
         logging.exception('Message cant be deleted')
         return True
 
-    if isinstance(exception, MessageToDeleteNotFound):
+    elif isinstance(exception, MessageToDeleteNotFound):
         logging.exception('Message to delete not found')
         return True
 
-    if isinstance(exception, MessageTextIsEmpty):
+    elif isinstance(exception, MessageTextIsEmpty):
         logging.exception('MessageTextIsEmpty')
         return True
 
-    if isinstance(exception, Unauthorized):
+    elif isinstance(exception, Unauthorized):
         logging.exception(f'Unauthorized: {exception}')
         return True
 
-    if isinstance(exception, InvalidQueryID):
+    elif isinstance(exception, InvalidQueryID):
         logging.exception(f'InvalidQueryID: {exception} \nUpdate: {update}')
         return True
 
-    if isinstance(exception, TelegramAPIError):
+    elif isinstance(exception, TelegramAPIError):
         logging.exception(f'TelegramAPIError: {exception} \nUpdate: {update}')
         return True
-    if isinstance(exception, RetryAfter):
+    elif isinstance(exception, RetryAfter):
         logging.exception(f'RetryAfter: {exception} \nUpdate: {update}')
         return True
-    if isinstance(exception, CantParseEntities):
+    elif isinstance(exception, CantParseEntities):
         logging.exception(f'CantParseEntities: {exception} \nUpdate: {update}')
         return True
 
