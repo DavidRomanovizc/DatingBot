37242d777852aff837f62942dfa045a0d39cfb56
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-30 23:32:33 +0300

    🚧 Django admin in progress development

---
+++ django_project/telegrambot/usersmanage/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import User
+
+
+# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "telegram_id", "username", "created_at")
---
+++ django_project/telegrambot/usersmanage/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+# Create your models here.
+class TimeBasedModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class User(TimeBasedModel):
+    class Meta:
+        verbose_name = "Пользователь",
+        verbose_name_plural = "Пользователи"
+
+    id = models.AutoField(primary_key=True)
+    telegram_id = models.BigIntegerField(unique=True, default=1, verbose_name="ID пользователя Телеграм")
+    name = models.CharField(max_length=255, verbose_name="Имя пользователя")
+    username = models.CharField(max_length=255, verbose_name="Username Telegram")
+    email = models.EmailField(max_length=255, verbose_name="Email", null=True)
+    age = models.PositiveIntegerField(verbose_name="Age")
+    commentary = models.TextField(max_length=300, verbose_name="About the user", null=True)
+    photo = models.CharField(verbose_name="Фото file_id", max_length=200)
+
+    def __str__(self):
+        return f"№{self.id} ({self.telegram_id}) - {self.name}"
+
+
