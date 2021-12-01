46511de0e08a51bf95d858c254772f2e2b4341c6
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-02 00:09:42 +0300

    ♻️ Refactor code

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import logging
+import os
+import django
 from aiogram import executor
-
+from django_project.telegrambot.telegrambot import settings
 from loader import dp, db, bot, storage
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
@@ -25,5 +27,15 @@ async def on_shutdown():
     await storage.close()
 
 
+def setup_django():
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "django_project.telegrambot.telegrambot.settings"
+    )
+    os.environ.update({"DJANGO_ALLOW_ASYNC_UNSAFE": "true"})
+    django.setup()
+
+
 if __name__ == '__main__':
+    setup_django()
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
