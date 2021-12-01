0937074758a881c45727689812fc5683c31b98f1
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-01 23:51:19 +0300

    🚧 Django in progress development

--- django_project/telegrambot/manage.py
+++ django_project/telegrambot/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telegrambot.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.telegrambot.telegrambot.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
