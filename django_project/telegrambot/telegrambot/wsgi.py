ed85a7f96703198342c09a8c34a113c0afd615de
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-03 21:49:31 +0300

    ✨ Work completed with django #5

--- django_project/telegrambot/telegrambot/wsgi.py
+++ django_project/telegrambot/telegrambot/wsgi.py
@@ -4,7 +4,6 @@ WSGI config for telegrambot project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
