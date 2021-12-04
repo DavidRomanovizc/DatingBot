d09530537a9724e6215056d7777c2800c1ca354a
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-04 17:07:38 +0300

    ✅ Update __init__

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -6,7 +6,7 @@ from . import change_datas
 from . import menu
 from . import find_handlers
 from . import back_handler
-from . import instruction
+from . import statistics
 from . import information
 from . import view
 from . import echo
