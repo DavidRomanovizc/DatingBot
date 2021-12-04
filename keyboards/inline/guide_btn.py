72bc65f13c240698ab41759de9d83303c70e4329
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-04 17:10:00 +0300

    ✨ Button for instructions

--- keyboards/inline/guide_btn.py
+++ keyboards/inline/guide_btn.py
@@ -1,13 +1,41 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-btn_guide = InlineKeyboardMarkup(row_width=3,
+first_str = InlineKeyboardMarkup(row_width=3,
+                                 inline_keyboard=[
+                                     [
+                                         InlineKeyboardButton(text='➡️ Вперед',
+                                                              callback_data='forward_f')
+                                     ],
+
+                                     [
+                                         InlineKeyboardButton(text='❌ Закрыть',
+                                                              callback_data='close_everything')
+                                     ],
+                                 ]
+                                 )
+
+second_str = InlineKeyboardMarkup(row_width=3,
+                                  inline_keyboard=[
+                                      [
+                                          InlineKeyboardButton(text='⬅️ Назад',
+                                                               callback_data='backward_s'),
+                                          InlineKeyboardButton(text='➡️ Вперед',
+                                                               callback_data='forward_s')
+
+                                      ],
+
+                                      [
+                                          InlineKeyboardButton(text='❌ Закрыть',
+                                                               callback_data='close_everything')
+                                      ],
+                                  ]
+                                  )
+
+third_str = InlineKeyboardMarkup(row_width=3,
                                  inline_keyboard=[
                                      [
                                          InlineKeyboardButton(text='⬅️ Назад',
-                                                              callback_data='backward'),
-                                         InlineKeyboardButton(text='➡️ Вперед',
-                                                              callback_data='forward')
-
+                                                              callback_data='backward_th'),
                                      ],
 
                                      [
