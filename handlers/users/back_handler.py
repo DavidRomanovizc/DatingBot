e5c861668fb4711739857f79abfd84ea8e53a200
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-28 22:58:24 +0300

    Deleted unused imports

--- handlers/users/back_handler.py
+++ handlers/users/back_handler.py
@@ -1,7 +1,7 @@
 from keyboards.inline.main_menu import inline_start
 from keyboards.inline.second_menu import menu_inline_kb
 from aiogram.types import CallbackQuery
-from loader import dp, bot, db
+from loader import dp, bot
 
 
 @dp.callback_query_handler(text="cancel")
@@ -27,7 +27,6 @@ async def back_second_menu(call: CallbackQuery):
 
 @dp.callback_query_handler(text="close_everything")
 async def get_close_everything(call: CallbackQuery):
-    count_users = await db.count_users()
     await bot.delete_message(chat_id=call.from_user.id, message_id=call.message.message_id)
     await call.message.answer("<b>❤️️ DATE_BOT</b> - платформа для поиска новых знакомств.\n\n"
                               "<b>🤝 Сотрудничество: </b>\n"
