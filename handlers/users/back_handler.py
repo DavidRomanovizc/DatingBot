a9f6b574ec307f87fe2a4dab9c793b560595c480
vr4eslav <kldgrcorp@gmail.com>
2022-03-13 20:49:43 +0300

    changes for back_handler (optimize)

--- handlers/users/back_handler.py
+++ handlers/users/back_handler.py
@@ -1,34 +1,13 @@
-from keyboards.inline.main_menu import inline_start
-from keyboards.inline.second_menu import menu_inline_kb
 from aiogram.types import CallbackQuery
-from loader import dp, bot
+
+from keyboards.inline.second_menu import menu_inline_kb
+from loader import dp
 
 
-@dp.callback_query_handler(text="cancel")
-async def cancel_buying(call: CallbackQuery):
-    await call.message.edit_text("<b>❤️️ DATE_BOT</b> - платформа для поиска новых знакомств\n\n"
-                                 "<b>🤝 Сотрудничество: </b>\n"
-                                 "Если у вас есть предложение о сотрудничестве, пишите сюда - "
-                                 "@DRomanovizc", reply_markup=inline_start)
+@dp.callback_query_handler(text="back_with_delete")
+async def open_menu(call: CallbackQuery):
+    await call.message.delete()
+    await call.message.answer(f"Меню: ",
+                              reply_markup=menu_inline_kb)
 
 
-@dp.callback_query_handler(text="submenu")
-async def back_to_menu(call: CallbackQuery):
-    await call.message.edit_text("<b>❤️️ DATE_BOT</b> - платформа для поиска новых знакомств\n\n"
-                                 "<b>🤝 Сотрудничество: </b>\n"
-                                 "Если у вас есть предложение о сотрудничестве, пишите сюда - "
-                                 "@DRomanovizc", reply_markup=inline_start)
-
-
-@dp.callback_query_handler(text="go_bac_to_second_menu")
-async def back_second_menu(call: CallbackQuery):
-    await call.message.edit_text("Вы были возвращены в меню: ", reply_markup=menu_inline_kb)
-
-
-@dp.callback_query_handler(text="close_everything")
-async def get_close_everything(call: CallbackQuery):
-    await bot.delete_message(chat_id=call.from_user.id, message_id=call.message.message_id)
-    await call.message.answer("<b>❤️️ DATE_BOT</b> - платформа для поиска новых знакомств.\n\n"
-                              "<b>🤝 Сотрудничество: </b>\n"
-                              "Если у вас есть предложение о сотрудничестве, пишите сюда - "
-                              "@DRomanovizc", reply_markup=inline_start)
