1d5101113a877b7b06afc2ae5dc8ac9dee9f99bb
vr4eslav <kldgrcorp@gmail.com>
2022-03-13 21:03:16 +0300

    mega refactor and FUCK CALL ANSWER

--- handlers/users/registration.py
+++ handlers/users/registration.py
@@ -1,39 +1,36 @@
-from keyboards.inline.lifestyle_choice_inline import lifestyle_inline_kb
-from keyboards.inline.second_menu import menu_inline_kb
-from aiogram.types import CallbackQuery, ContentType
-from keyboards.inline.sex_partner import btn_partner
-from keyboards.inline.profile_bt import reg_profile
-from aiogram.dispatcher import FSMContext
-from states.reg_state import RegData
-from loader import dp, bot, db
-from aiogram import types
 import asyncpg
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.types import CallbackQuery, ContentType
+from loguru import logger
+
+from keyboards.inline.lifestyle_choice_inline import lifestyle_inline_kb
+from keyboards.inline.profile_bt import reg_profile
+from keyboards.inline.second_menu import menu_inline_kb
+from keyboards.inline.sex_partner import btn_partner
+from loader import dp, db
+from states.reg_state import RegData
 
 
 @dp.callback_query_handler(text='registration')
 async def registration(call: CallbackQuery):
-    await call.answer(cache_time=60)
     text = f"Пройдите опрос, чтобы зарегистрироваться"
-    await bot.send_message(call.from_user.id, text, reply_markup=reg_profile)
+    await call.message.edit_text(text, reply_markup=reg_profile)
 
 
 @dp.callback_query_handler(text_contains="survey")
 async def survey(call: CallbackQuery):
-    await call.answer(cache_time=60)
-    keyboard = types.InlineKeyboardMarkup()
+    keyboard = types.InlineKeyboardMarkup(row_with=1)
     btn1 = types.InlineKeyboardButton(text='Мужской', callback_data='male_reg')
-    keyboard.add(btn1)
     btn2 = types.InlineKeyboardButton(text='Женский', callback_data='female_reg')
-    keyboard.add(btn2)
+    keyboard.add(btn1, btn2)
 
-    await bot.send_message(call.from_user.id, "Выберите пол", reply_markup=keyboard)
+    await call.message.edit_text("Выберите пол", reply_markup=keyboard)
     await RegData.sex.set()
 
 
-@dp.callback_query_handler(text='male_reg', state=RegData.sex)
-@dp.callback_query_handler(text='female_reg', state=RegData.sex)
+@dp.callback_query_handler(state=RegData.sex)
 async def sex_reg(call: CallbackQuery):
-    await call.answer(cache_time=60)
     if call.data == 'male_reg':
         try:
             await db.update_user_sex(telegram_id=call.from_user.id, sex='Мужской')
@@ -45,7 +42,7 @@ async def sex_reg(call: CallbackQuery):
         except asyncpg.exceptions.UniqueViolationError as err:
             print(err)
 
-    await bot.send_message(call.from_user.id, f'Теперь напишите немного о себе: \n\n(255 символов max.)')
+    await call.message.edit_text(f'Теперь напишите немного о себе: \n\n(255 символов max.)')
     await RegData.commentary.set()
 
 
@@ -53,18 +50,17 @@ async def sex_reg(call: CallbackQuery):
 async def commentary_reg(message: types.Message):
     try:
         await db.update_user_commentary(commentary=message.text, telegram_id=message.from_user.id)
-        await message.reply(f'Комментарий принят!')
-    except:
+        await message.reply(f'Комментарий принят! Выберите, кого вы хотите найти: ', reply_markup=btn_partner)
+    except Exception as err:
+        logger.error(err)
         await message.reply(f'Произошла неизвестная ошибка! Попробуйте изменить комментарий позже в разделе '
-                            f'"Меню"')
-    await bot.send_message(message.from_user.id, f'Выберите, кого вы хотите найти: ', reply_markup=btn_partner)
+                            f'"Меню"\n\n'
+                            f'Выберите, кого вы хотите найти: ', reply_markup=btn_partner)
     await RegData.need_partner_sex.set()
 
 
-@dp.callback_query_handler(text='gen_male', state=RegData.need_partner_sex)
-@dp.callback_query_handler(text='g_fe', state=RegData.need_partner_sex)
+@dp.callback_query_handler(state=RegData.need_partner_sex)
 async def sex_reg(call: CallbackQuery):
-    await call.answer(cache_time=60)
     if call.data == 'gen_male':
         try:
             await db.update_user_need_partner_sex(telegram_id=call.from_user.id, need_partner_sex='Мужской')
@@ -76,8 +72,7 @@ async def sex_reg(call: CallbackQuery):
         except asyncpg.exceptions.UniqueViolationError as err:
             print(err)
 
-    await bot.send_message(call.from_user.id,
-                           f'Отлично! Теперь напишите мне ваше имя, которое будут все видеть в анкете')
+    await call.message.edit_text(f'Отлично! Теперь напишите мне ваше имя, которое будут все видеть в анкете')
     await RegData.name.set()
 
 
@@ -88,7 +83,7 @@ async def get_name(message: types.Message, state: FSMContext):
         await db.update_user_varname(telegram_id=message.from_user.id, varname=message.text)
     except asyncpg.exceptions.UniqueViolationError as err:
         print(err)
-    await bot.send_message(message.from_user.id, "Введите сколько вам лет:")
+    await message.reply("Введите сколько вам лет:")
     await RegData.age.set()
 
 
@@ -99,17 +94,16 @@ async def get_age(message: types.Message, state: FSMContext):
         await db.update_user_age(telegram_id=message.from_user.id, age=message.text)
     except asyncpg.exceptions.UniqueViolationError as err:
         print(err)
-    await bot.send_message(message.from_user.id, "Введите вашу национальность:")
+    await message.reply("Введите вашу национальность:")
     await RegData.nationality.set()
 
 
 @dp.message_handler(state=RegData.nationality)
 async def get_nationality(message: types.Message, state: FSMContext):
-    keyboard = types.InlineKeyboardMarkup()
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
     btn1 = types.InlineKeyboardButton(text='Высшее', callback_data='higher_edu')
-    keyboard.add(btn1)
     btn2 = types.InlineKeyboardButton(text='Среднее', callback_data='secondary_edu')
-    keyboard.add(btn2)
+    keyboard.add(btn1, btn2)
     await state.update_data(nationality=message.text)
 
     try:
@@ -117,14 +111,12 @@ async def get_nationality(message: types.Message, state: FSMContext):
         await state.update_data(nationality=message.text)
     except asyncpg.exceptions.UniqueViolationError as err:
         print(err)
-    await bot.send_message(message.from_user.id, "Введите ваше образование:", reply_markup=keyboard)
+    await message.reply("Введите ваше образование:", reply_markup=keyboard)
     await RegData.education.set()
 
 
-@dp.callback_query_handler(text='higher_edu', state=RegData.education)
-@dp.callback_query_handler(text='secondary_edu', state=RegData.education)
+@dp.callback_query_handler(state=RegData.education)
 async def get_education(call: CallbackQuery, state=FSMContext):
-    await call.answer(cache_time=60)
     if call.data == 'higher_edu':
         try:
             await db.update_user_education(telegram_id=call.from_user.id, education='Высшее')
@@ -137,35 +129,31 @@ async def get_education(call: CallbackQuery, state=FSMContext):
             await state.update_data(education="Среднее")
         except asyncpg.exceptions.UniqueViolationError as err:
             print(err)
-    await bot.send_message(chat_id=call.from_user.id, text="Введите город в котором проживаете:")
+    await call.message.edit_text(text="Введите город в котором проживаете:")
     await RegData.town.set()
 
 
 @dp.message_handler(state=RegData.town)
 async def get_town(message: types.Message, state: FSMContext):
-    keyboard = types.InlineKeyboardMarkup()
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
     btn1 = types.InlineKeyboardButton(text='Да', callback_data='car_true')
-    keyboard.add(btn1)
     btn2 = types.InlineKeyboardButton(text='Нет', callback_data='car_false')
-    keyboard.add(btn2)
+    keyboard.add(btn1, btn2)
     try:
         await db.update_user_city(telegram_id=message.from_user.id, city=message.text)
         await state.update_data(town=message.text)
     except asyncpg.exceptions.UniqueViolationError as err:
         print(err)
-    await bot.send_message(message.from_user.id, "Имеете ли вы машину:", reply_markup=keyboard)
+    await message.reply("Имеете ли вы машину:", reply_markup=keyboard)
     await RegData.car.set()
 
 
-@dp.callback_query_handler(text=['car_true'], state=RegData.car)
-@dp.callback_query_handler(text=['car_false'], state=RegData.car)
+@dp.callback_query_handler(state=RegData.car)
 async def get_car(call: CallbackQuery, state: FSMContext):
-    await call.answer(cache_time=60)
-    keyboard = types.InlineKeyboardMarkup()
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
     btn1 = types.InlineKeyboardButton(text='Да', callback_data='apart_true')
-    keyboard.add(btn1)
     btn2 = types.InlineKeyboardButton(text='Нет', callback_data='apart_false')
-    keyboard.add(btn2)
+    keyboard.add(btn1, btn2)
     if call.data == 'car_true':
         try:
             await db.update_user_car(telegram_id=call.from_user.id, car=True)
@@ -178,14 +166,12 @@ async def get_car(call: CallbackQuery, state: FSMContext):
             await state.update_data(car='Нет машины')
         except asyncpg.exceptions.UniqueViolationError as err:
             print(err)
-    await bot.send_message(call.from_user.id, "Имеете ли вы свое жилье:", reply_markup=keyboard)
+    await call.message.edit_text("Имеете ли вы свое жилье:", reply_markup=keyboard)
     await RegData.own_home.set()
 
 
-@dp.callback_query_handler(text_contains=['apart_true'], state=RegData.own_home)
-@dp.callback_query_handler(text_contains=['apart_false'], state=RegData.own_home)
+@dp.callback_query_handler(state=RegData.own_home)
 async def get_own_home(call: CallbackQuery, state: FSMContext):
-    await call.answer(cache_time=60)
     if call.data == 'apart_true':
         try:
             await db.update_user_apartment(telegram_id=call.from_user.id, apartment=True)
@@ -198,25 +184,16 @@ async def get_own_home(call: CallbackQuery, state: FSMContext):
             await state.update_data(own_home='Нет квартиры')
         except asyncpg.exceptions.UniqueViolationError as err:
             print(err)
-    await bot.send_message(call.from_user.id, "Чем вы занимаетесь:", reply_markup=lifestyle_inline_kb)
+    await call.message.edit_text("Чем вы занимаетесь:", reply_markup=lifestyle_inline_kb)
     await RegData.hobbies.set()
 
 
-@dp.callback_query_handler(state=RegData.hobbies,
-                           text_contains=['study_lifestyle'])
-@dp.callback_query_handler(state=RegData.hobbies,
-                           text_contains=['work_lifestyle'])
-@dp.callback_query_handler(state=RegData.hobbies,
-                           text_contains=['job_find_lifestyle'])
-@dp.callback_query_handler(state=RegData.hobbies,
-                           text_contains=['householder_lifestyle'])
+@dp.callback_query_handler(state=RegData.hobbies)
 async def get_hobbies(call: CallbackQuery, state: FSMContext):
-    await call.answer(cache_time=60)
-    keyboard = types.InlineKeyboardMarkup()
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
     btn1 = types.InlineKeyboardButton(text='Занят', callback_data='busy')
-    keyboard.add(btn1)
     btn2 = types.InlineKeyboardButton(text='Не занят', callback_data='not_busy')
-    keyboard.add(btn2)
+    keyboard.add(btn1, btn2)
 
     if call.data == 'study_lifestyle':
         try:
@@ -242,19 +219,16 @@ async def get_hobbies(call: CallbackQuery, state: FSMContext):
             await state.update_data(hobbies='Домохозяйка/Домохозяин')
         except asyncpg.exceptions.UniqueViolationError as err:
             print(err)
-    await bot.send_message(call.from_user.id, "Выберите ваше семейное положение", reply_markup=keyboard)
+    await call.message.edit_text("Выберите ваше семейное положение", reply_markup=keyboard)
     await RegData.marital.set()
 
 
-@dp.callback_query_handler(text_contains='busy', state=RegData.marital)
-@dp.callback_query_handler(text_contains='not_busy', state=RegData.marital)
+@dp.callback_query_handler(state=RegData.marital)
 async def get_marital(call: CallbackQuery, state=FSMContext):
-    await call.answer(cache_time=60)
-    keyboard = types.InlineKeyboardMarkup()
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
     btn1 = types.InlineKeyboardButton(text='Да', callback_data='true')
-    keyboard.add(btn1)
     btn2 = types.InlineKeyboardButton(text='Нет', callback_data='false')
-    keyboard.add(btn2)
+    keyboard.add(btn1, btn2)
 
     if call.data == 'busy':
         try:
@@ -269,14 +243,12 @@ async def get_marital(call: CallbackQuery, state=FSMContext):
         except asyncpg.exceptions.UniqueViolationError as err:
             print(err)
 
-    await bot.send_message(call.from_user.id, "Есть ли у вас дети?", reply_markup=keyboard)
+    await call.message.edit_text("Есть ли у вас дети?", reply_markup=keyboard)
     await RegData.child.set()
 
 
-@dp.callback_query_handler(text_contains=['true'], state=RegData.child)
-@dp.callback_query_handler(text_contains=['false'], state=RegData.child)
+@dp.callback_query_handler(state=RegData.child)
 async def get_children(call: CallbackQuery, state=FSMContext):
-    await call.answer(cache_time=60)
     if call.data == 'true':
         try:
             await db.update_user_kids(telegram_id=call.from_user.id, kids=True)
@@ -289,7 +261,7 @@ async def get_children(call: CallbackQuery, state=FSMContext):
             await state.update_data(child='Нет детей')
         except asyncpg.exceptions.UniqueViolationError as err:
             print(err)
-    await bot.send_message(call.from_user.id, f'И напоследок, отправьте мне Вашу фотографию')
+    await call.message.edit_text(f'И напоследок, отправьте мне Вашу фотографию')
     await RegData.photo.set()
 
 
@@ -299,7 +271,8 @@ async def get_photo(message: types.Message, state: FSMContext):
     try:
         await db.update_user_photo_id(photo_id=file_id, telegram_id=message.from_user.id)
         await message.reply(f'Фото принято!')
-    except:
+    except Exception as err:
+        logger.error(err)
         await message.reply(f'Произошла ошибка! Попробуйте еще раз либо отправьте другую фотографию. \n'
                             f'Если ошибка осталась, напишите системному администратору.')
 
@@ -338,20 +311,18 @@ async def get_photo(message: types.Message, state: FSMContext):
     user_marital = user.get('marital')
     user_comm = user.get('commentary')
 
-    await bot.send_message(message.from_user.id, "Вы успешно зарегистрированы")
-    await bot.send_photo(
-        message.from_user.id, caption=f"1. Ваше имя - {str(user_name)}\n"
-                                      f"2. Ваш возраст - {str(user_age)}\n"
-                                      f"3. Ваш пол - {str(user_sex)}\n"
-                                      f"4. Ваша национальность - {str(user_national)}\n"
-                                      f"5. Ваше образование - {str(user_education)}\n"
-                                      f"6. Ваш город - {str(user_city)}\n"
-                                      f"7. Наличие машины - {str(user_car)}\n"
-                                      f"8. Наличие жилья - {str(user_apart)}\n"
-                                      f"9. Ваше занятие - {str(user_life_style)}\n"
-                                      f"10. Наличие детей - {str(user_kids)}\n"
-                                      f"11. Семейное положение - {str(user_marital)}\n\n"
-                                      f"12. О себе - {str(user_comm)}",
-        photo=user.get('photo_id')
-    )
-    await message.answer("Меню: ", reply_markup=menu_inline_kb)
+    await message.answer_photo(caption=f"Регистрация завершена успешно! \n\n "
+                                       f"1. Ваше имя - {str(user_name)}\n"
+                                       f"2. Ваш возраст - {str(user_age)}\n"
+                                       f"3. Ваш пол - {str(user_sex)}\n"
+                                       f"4. Ваша национальность - {str(user_national)}\n"
+                                       f"5. Ваше образование - {str(user_education)}\n"
+                                       f"6. Ваш город - {str(user_city)}\n"
+                                       f"7. Наличие машины - {str(user_car)}\n"
+                                       f"8. Наличие жилья - {str(user_apart)}\n"
+                                       f"9. Ваше занятие - {str(user_life_style)}\n"
+                                       f"10. Наличие детей - {str(user_kids)}\n"
+                                       f"11. Семейное положение - {str(user_marital)}\n\n"
+                                       f"12. О себе - {str(user_comm)}\n\n"
+                                       f"Меню: ",
+                               photo=user.get('photo_id'), reply_markup=menu_inline_kb)
