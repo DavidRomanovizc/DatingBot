0cd005922aaddd233842a7a38f9bba11163a4ee2
David <pppnpppp2@gmail.com>
2022-04-03 00:46:27 +0300

    ♻ Changed `print` to `logger`

--- handlers/users/registration.py
+++ handlers/users/registration.py
@@ -2,7 +2,6 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.types import CallbackQuery, ContentType
 
-from handlers.users.back_handler import delete_message
 from keyboards.inline.lifestyle_choice_inline import lifestyle_keyboard
 from keyboards.inline.registration_inline import gender_keyboard, education_keyboard, town_keyboard, car_keyboard, \
     hobbies_keyboard, family_keyboard
@@ -10,8 +9,11 @@ from keyboards.inline.second_menu_inline import second_menu_keyboard
 from keyboards.inline.profile_inline import registration_keyboard
 from keyboards.inline.gender_inline import sex_partner
 
-from states.reg_state import RegData
+from utils.misc.create_questionnaire import get_data
 from utils.db_api import db_commands
+
+from states.reg_state import RegData
+
 from loguru import logger
 from loader import dp
 import asyncpg
@@ -38,12 +40,12 @@ async def sex_reg(call: CallbackQuery):
         try:
             await db_commands.update_user_data(telegram_id=call.from_user.id, sex="Мужской")
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     elif call.data == 'female_reg':
         try:
             await db_commands.update_user_data(telegram_id=call.from_user.id, sex='Женский')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
 
     await call.message.edit_text(f'Теперь напишите немного о себе: \n\n(255 символов max.)')
     await RegData.commentary.set()
@@ -69,12 +71,12 @@ async def sex_reg(call: CallbackQuery):
         try:
             await db_commands.update_user_data(telegram_id=call.from_user.id, need_partner_sex='Мужской')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     elif call.data == 'g_fe':
         try:
             await db_commands.update_user_data(telegram_id=call.from_user.id, need_partner_sex='Женский')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
 
     await call.message.edit_text(f'Отлично! Теперь напишите мне ваше имя, которое будут все видеть в анкете')
     await RegData.name.set()
@@ -86,7 +88,7 @@ async def get_name(message: types.Message, state: FSMContext):
     try:
         await db_commands.update_user_data(telegram_id=message.from_user.id, varname=message.text)
     except asyncpg.exceptions.UniqueViolationError as err:
-        print(err)
+        logger.error(err)
     await message.answer("Введите сколько вам лет:")
     await RegData.age.set()
 
@@ -97,7 +99,7 @@ async def get_age(message: types.Message, state: FSMContext):
     try:
         await db_commands.update_user_data(telegram_id=message.from_user.id, age=message.text)
     except asyncpg.exceptions.UniqueViolationError as err:
-        print(err)
+        logger.error(err)
     await message.answer("Введите вашу национальность:")
     await RegData.nationality.set()
 
@@ -110,7 +112,7 @@ async def get_nationality(message: types.Message, state: FSMContext):
         await db_commands.update_user_data(telegram_id=message.from_user.id, national=message.text)
         await state.update_data(nationality=message.text)
     except asyncpg.exceptions.UniqueViolationError as err:
-        print(err)
+        logger.error(err)
     await message.answer("Введите ваше образование:", reply_markup=markup)
     await RegData.education.set()
 
@@ -122,13 +124,13 @@ async def get_education(call: CallbackQuery, state=FSMContext):
             await db_commands.update_user_data(telegram_id=call.from_user.id, education='Высшее')
             await state.update_data(education='Высшее')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     elif call.data == 'secondary_edu':
         try:
             await db_commands.update_user_data(telegram_id=call.from_user.id, education='Среднее')
             await state.update_data(education="Среднее")
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     await call.message.edit_text(text="Введите город в котором проживаете:")
     await RegData.town.set()
 
@@ -140,7 +142,7 @@ async def get_town(message: types.Message, state: FSMContext):
         await db_commands.update_user_data(telegram_id=message.from_user.id, city=message.text)
         await state.update_data(town=message.text)
     except asyncpg.exceptions.UniqueViolationError as err:
-        print(err)
+        logger.error(err)
     await message.answer("Имеете ли вы машину:", reply_markup=markup)
     await RegData.car.set()
 
@@ -153,13 +155,13 @@ async def get_car(call: CallbackQuery, state: FSMContext):
             await db_commands.update_user_data(telegram_id=call.from_user.id, car=True)
             await state.update_data(car='Есть машина')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     elif call.data == 'car_false':
         try:
             await db_commands.update_user_data(telegram_id=call.from_user.id, car=False)
             await state.update_data(car='Нет машины')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     await call.message.edit_text("Имеете ли вы свое жилье:", reply_markup=markup)
     await RegData.own_home.set()
 
@@ -172,13 +174,13 @@ async def get_own_home(call: CallbackQuery, state: FSMContext):
             await db_commands.update_user_data(telegram_id=call.from_user.id, apartment=True)
             await state.update_data(own_home='Есть квартира')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     elif call.data == 'apart_false':
         try:
             await db_commands.update_user_data(telegram_id=call.from_user.id, apartment=False)
             await state.update_data(own_home='Нет квартиры')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     await call.message.edit_text("Чем вы занимаетесь:", reply_markup=markup)
     await RegData.hobbies.set()
 
@@ -191,25 +193,25 @@ async def get_hobbies(call: CallbackQuery, state: FSMContext):
             await db_commands.update_user_data(telegram_id=call.from_user.id, lifestyle='Учусь')
             await state.update_data(hobbies='Учусь')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     elif call.data == 'work_lifestyle':
         try:
             await db_commands.update_user_data(telegram_id=call.from_user.id, lifestyle='Работаю')
             await state.update_data(hobbies='Работаю')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     elif call.data == 'job_find_lifestyle':
         try:
             await db_commands.update_user_data(telegram_id=call.from_user.id, lifestyle='Ищу работу')
             await state.update_data(hobbies='Ищу работу')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     elif call.data == 'householder_lifestyle':
         try:
             await db_commands.update_user_data(telegram_id=call.from_user.id, lifestyle='Домохозяйка/Домохозяин')
             await state.update_data(hobbies='Домохозяйка/Домохозяин')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     await call.message.edit_text("Выберите ваше семейное положение", reply_markup=markup)
     await RegData.marital.set()
 
@@ -223,13 +225,13 @@ async def get_marital(call: CallbackQuery, state=FSMContext):
             await db_commands.update_user_data(telegram_id=call.from_user.id, marital='Занят/a')
             await state.update_data(marital='Занят')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     elif call.data == 'not_busy':
         try:
             await db_commands.update_user_data(telegram_id=call.from_user.id, marital='Не занят/a')
             await state.update_data(marital='Не занят')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
 
     await call.message.edit_text("Есть ли у вас дети?", reply_markup=markup)
     await RegData.child.set()
@@ -242,13 +244,13 @@ async def get_children(call: CallbackQuery, state=FSMContext):
             await db_commands.update_user_data(telegram_id=call.from_user.id, kids=True)
             await state.update_data(child='Есть дети')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     elif call.data == 'false':
         try:
             await db_commands.update_user_data(telegram_id=call.from_user.id, kids=False)
             await state.update_data(child='Нет детей')
         except asyncpg.exceptions.UniqueViolationError as err:
-            print(err)
+            logger.error(err)
     await call.message.edit_text(f'И напоследок, отправьте мне Вашу фотографию')
     await RegData.photo.set()
 
@@ -256,6 +258,7 @@ async def get_children(call: CallbackQuery, state=FSMContext):
 @dp.message_handler(content_types=ContentType.PHOTO, state=RegData.photo)
 async def get_photo(message: types.Message, state: FSMContext):
     file_id = message.photo[-1].file_id
+    markup = await second_menu_keyboard()
     try:
         await db_commands.update_user_data(photo_id=file_id, telegram_id=message.from_user.id)
         await message.reply(f'Фото принято!')
@@ -266,51 +269,23 @@ async def get_photo(message: types.Message, state: FSMContext):
 
     await state.finish()
 
-    user = await db_commands.select_user(telegram_id=message.from_user.id)
-
-    user_name = user.get('varname')
-    user_age = user.get('age')
-    user_sex = user.get('sex')
-    user_national = user.get('national')
-    user_education = user.get('education')
-    user_city = user.get('city')
-
-    user_car = user.get('car')
-    if user_car:
-        user_car = 'Есть машина'
-    else:
-        user_car = 'Нет машины'
-
-    user_apart = user.get('apartment')
-    if user_apart:
-        user_apart = 'Есть квартира'
-    else:
-        user_apart = 'Нет квартиры'
-
-    user_life_style = user.get('lifestyle')
-
-    user_kids = user.get('kids')
-    if user_kids:
-        user_kids = 'Есть дети'
-    else:
-        user_kids = 'Нет детей'
-
-    user_marital = user.get('marital')
-    user_comm = user.get('commentary')
-    markup = await second_menu_keyboard()
+    telegram_id = message.from_user.id
+    user_data = await get_data(telegram_id)
+    user = await db_commands.select_user(telegram_id=telegram_id)
     await message.answer_photo(caption=f"Регистрация завершена успешно! \n\n "
-                                       f"1. Ваше имя - {str(user_name)}\n"
-                                       f"2. Ваш возраст - {str(user_age)}\n"
-                                       f"3. Ваш пол - {str(user_sex)}\n"
-                                       f"4. Ваша национальность - {str(user_national)}\n"
-                                       f"5. Ваше образование - {str(user_education)}\n"
-                                       f"6. Ваш город - {str(user_city)}\n"
-                                       f"7. Наличие машины - {str(user_car)}\n"
-                                       f"8. Наличие жилья - {str(user_apart)}\n"
-                                       f"9. Ваше занятие - {str(user_life_style)}\n"
-                                       f"10. Наличие детей - {str(user_kids)}\n"
-                                       f"11. Семейное положение - {str(user_marital)}\n\n"
-                                       f"12. О себе - {str(user_comm)}\n\n",
+                                       f"Статус анкеты - {str(user_data[12])}\n\n"
+                                       f"1. Ваше имя - {str(user_data[0])}\n"
+                                       f"2. Ваш возраст - {str(user_data[1])}\n"
+                                       f"3. Ваш пол - {str(user_data[2])}\n"
+                                       f"4. Ваша национальность - {str(user_data[3])}\n"
+                                       f"5. Ваше образование - {str(user_data[4])}\n"
+                                       f"6. Ваш город - {str(user_data[5])}\n"
+                                       f"7. Наличие машины - {str(user_data[6])}\n"
+                                       f"8. Наличие жилья - {str(user_data[7])}\n"
+                                       f"9. Ваше занятие - {str(user_data[8])}\n"
+                                       f"10. Наличие детей - {str(user_data[9])}\n"
+                                       f"11. Семейное положение - {str(user_data[10])}\n\n"
+                                       f"12. О себе - {str(user_data[11])}\n\n",
                                photo=user.get('photo_id'))
 
     await message.answer("Меню: ", reply_markup=markup)
