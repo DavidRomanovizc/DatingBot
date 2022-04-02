888549e598d9c78da7d0fa117c330ad8d72edb7a
David <pppnpppp2@gmail.com>
2022-04-02 13:07:17 +0300

    Update init, buttons

--- keyboards/inline/filters_inline.py
+++ keyboards/inline/filters_inline.py
@@ -9,12 +9,11 @@ async def filters_keyboard():
     search_name = InlineKeyboardButton(text="Поиск по имени", callback_data=filters_data_kb.new(checker="name"))
     search_age = InlineKeyboardButton(text="Поиск по возрасту", callback_data=filters_data_kb.new(checker="age"))
     search_city = InlineKeyboardButton(text="Поиск по городу", callback_data=filters_data_kb.new(checker="city"))
-    search_gender = InlineKeyboardButton(text="Поиск по полу", callback_data=filters_data_kb.new(checker="gender"))
     search_nationality = InlineKeyboardButton(text="Поиск по национальности",
                                               callback_data=filters_data_kb.new(checker="nationality"))
-    back_to_menu = InlineKeyboardButton(text="⏪️ Вернуться в меню", callback_data="start_menu")
+    back_to_menu = InlineKeyboardButton(text="⏪️ Вернуться в меню", callback_data="second_m")
     markup.row(search_name, search_age)
     markup.add(search_city)
-    markup.row(search_gender, search_nationality)
+    markup.row(search_nationality)
     markup.add(back_to_menu)
     return markup
