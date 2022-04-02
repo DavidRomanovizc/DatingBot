ea819daa84b093c9b24946601668613bbe5f4eea
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2022-04-02 23:52:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
 
-
 async def on_startup(dp):
     await db.create()
     await db.create_table_users()
