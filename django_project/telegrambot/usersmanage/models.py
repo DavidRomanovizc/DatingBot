45356173a7ad5e25616ac6c1c8e2f2ded46909b7
DavidRomanovizc <dtdzhalaev@gmail.com>
2022-04-24 13:01:26 +0300

    🗃️ Update database

--- django_project/telegrambot/usersmanage/models.py
+++ django_project/telegrambot/usersmanage/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
-
 # Create your models here.
+from sqlalchemy import null
+
+
 class TimeBasedModel(models.Model):
     class Meta:
         abstract = True
@@ -21,8 +23,10 @@ class User(TimeBasedModel):
     username = models.CharField(max_length=255, verbose_name="Username Telegram")
     email = models.EmailField(max_length=255, verbose_name="Email", null=True)
     sex = models.CharField(max_length=30, verbose_name="Пол искателя", null=True)
-    age = models.BigIntegerField(verbose_name="Возраст искателя", default=18)
-    city = models.CharField(max_length=40, verbose_name="Город искателя", null=True)
+    age = models.BigIntegerField(verbose_name="Возраст искателя", default=16)
+    city = models.CharField(max_length=255, verbose_name="Город искателя", null=True)
+    longitude = models.FloatField(verbose_name="координаты пользователя", null=True)
+    latitude = models.FloatField(verbose_name="координаты пользователя", null=True)
     verification = models.BooleanField(verbose_name="Верификация", default=False)
     language = models.CharField(max_length=10, verbose_name="Язык пользователя", null=True)
     varname = models.CharField(max_length=100, verbose_name="Публичное имя пользователя", null=True)
@@ -30,10 +34,16 @@ class User(TimeBasedModel):
     is_banned = models.BooleanField(verbose_name="Забанен ли пользователь", default=False)
     photo_id = models.CharField(max_length=400, verbose_name="Photo_ID", null=True)
     commentary = models.CharField(max_length=300, verbose_name="Комментарий пользователя", null=True)
-    need_partner_sex = models.CharField(max_length=50, verbose_name="Пол цели", null=True)
-    likes = models.BigIntegerField(verbose_name="Количество лайков у пользователя", default=0)
-    dislikes = models.BigIntegerField(verbose_name="Количество дизлайков у пользователя", default=0)
+    need_partner_sex = models.CharField(max_length=50, verbose_name="Пол партнера", null=True)
+    need_partner_age_min = models.IntegerField(verbose_name="Минимальный возраст партнера", default=16)
+    need_partner_age_max = models.IntegerField(verbose_name="Максимальный возраст партнера", default=24)
+    need_partner_range = models.CharField(max_length=100, verbose_name="Расстояние от пользователя", null=True,
+                                          default=300)
+    like = models.BigIntegerField(verbose_name="Количество лайков у пользователя", default=0)
+    dislike = models.BigIntegerField(verbose_name="Количество дизлайков у пользователя", default=0)
     phone_number = models.BigIntegerField(verbose_name="Номер телефона", null=True)
+    status = models.BooleanField(verbose_name="Статус анкеты", null=True, default=False)
+    instagram = models.CharField(max_length=30, verbose_name="Ник в инстаграме", null=True)
 
     def __str__(self):
         return f"№{self.id} ({self.telegram_id}) - {self.name}"
