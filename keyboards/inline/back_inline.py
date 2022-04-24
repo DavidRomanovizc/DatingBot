850dc914c8b225d38bd85d71b65cf3e873725836
DavidRomanovizc <dtdzhalaev@gmail.com>
2022-04-24 12:52:27 +0300

    🎨 Added a separate "back" button

---
+++ keyboards/inline/back_inline.py
@@ -0,0 +1,8 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+async def only_back_keyboard():
+    markup = InlineKeyboardMarkup()
+    back = InlineKeyboardButton(text="⏪️ Назад", callback_data="start_menu")
+    markup.add(back)
+    return markup
