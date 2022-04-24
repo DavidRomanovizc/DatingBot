d3e0fec6b129f5efdabe63fa0a207278da3e5bf3
DavidRomanovizc <dtdzhalaev@gmail.com>
2022-04-24 12:50:19 +0300

    🔥 Remove files

--- keyboards/inline/change_inline.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-async def gender_keyboard():
-    markup = InlineKeyboardMarkup(row_width=1)
-    male = InlineKeyboardButton(text='Мужской', callback_data='male')
-    female = InlineKeyboardButton(text='Женский', callback_data='female')
-    markup.add(male, female)
-    return markup
