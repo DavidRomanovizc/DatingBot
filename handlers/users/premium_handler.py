c2d8d25b1c2bb18285b5dcebc72e297d91246407
DavidRomanovizc <dtdzhalaev@gmail.com>
2022-05-20 22:58:00 +0300

    🔥 Delete unused code

--- handlers/users/premium_handler.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import CallbackQuery
-
-from loader import dp
-
-
-@dp.callback_query_handler(text="premium")
-async def deposit_balance(call: CallbackQuery):
-    await call.answer("Coming soon...")
