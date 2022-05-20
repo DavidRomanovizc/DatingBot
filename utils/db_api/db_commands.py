6d3f557ea361cb24856923cc4770a17491606dd4
DavidRomanovizc <dtdzhalaev@gmail.com>
2022-05-20 23:02:34 +0300

    🗃️ Added new functions

--- utils/db_api/db_commands.py
+++ utils/db_api/db_commands.py
@@ -1,7 +1,5 @@
 from asgiref.sync import sync_to_async
-
-
-from django_project.telegrambot.usersmanage.models import User
+from django_project.telegrambot.usersmanage.models import User, UserMeetings
 
 
 @sync_to_async
@@ -13,6 +11,23 @@ def select_user(telegram_id: int):
 @sync_to_async
 def add_user(telegram_id, name, username):
     return User(telegram_id=int(telegram_id), name=name, username=username).save()
+
+
+@sync_to_async
+def add_meetings_user(telegram_id, username):
+    return UserMeetings(telegram_id=int(telegram_id), username=username).save()
+
+
+@sync_to_async
+def select_all_user_meetings():
+    users = UserMeetings.objects.all().values()
+    return users
+
+
+@sync_to_async
+def select_user_meetings(telegram_id: int):
+    user = UserMeetings.objects.filter(telegram_id=telegram_id).values().first()
+    return user
 
 
 @sync_to_async
@@ -35,6 +50,17 @@ def count_users():
 @sync_to_async
 def update_user_data(telegram_id, **kwargs):
     return User.objects.filter(telegram_id=telegram_id).update(**kwargs)
+
+
+@sync_to_async
+def update_user_meetings_data(telegram_id, **kwargs):
+    return UserMeetings.objects.filter(telegram_id=telegram_id).update(**kwargs)
+
+
+@sync_to_async
+def select_meetings_user(telegram_id: int):
+    user = UserMeetings.objects.filter(telegram_id=telegram_id).values().first()
+    return user
 
 
 @sync_to_async
