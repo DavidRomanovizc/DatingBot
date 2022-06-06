6c7623c934072ee3fb9a46d391ec062244a4d88d
vr4eslav <kldgrcorp@gmail.com>
2022-06-06 12:16:55 +0300

    fix target destinations with callback data and with deleting states

--- utils/misc/create_questionnaire.py
+++ utils/misc/create_questionnaire.py
@@ -73,7 +73,8 @@ async def find_user_gender(telegram_id):
     return user_list
 
 
-async def send_questionnaire(chat_id, user_data, markup=None, add_text=None):
+async def send_questionnaire(chat_id, user_data, user_db, markup=None, add_text=None):
+    user_telegram_id = user_db.get("telegram_id")
     if add_text is None:
         await bot.send_photo(chat_id=chat_id, caption=f"<b>Статус анкеты</b> - \n{str(user_data[6])}\n\n"
                                                       f"<b>Имя</b> - {str(user_data[0])}\n"
@@ -82,7 +83,7 @@ async def send_questionnaire(chat_id, user_data, markup=None, add_text=None):
                                                       f"<b>Город</b> - {str(user_data[3])}\n"
                                                       f"<b>Ваше занятие</b> - {str(user_data[4])}\n"
                                                       f"<b>О себе</b> - {str(user_data[5])}\n\n",
-                             photo=user_data[7], reply_markup=await questionnaires_keyboard())
+                             photo=user_data[7], reply_markup=await questionnaires_keyboard(target_id=user_telegram_id))
     elif markup is None:
         await bot.send_photo(chat_id=chat_id, caption=f"{add_text}\n\n"
                                                       f"<b>Статус анкеты</b> - \n{str(user_data[6])}\n\n"
@@ -103,20 +104,20 @@ async def send_questionnaire(chat_id, user_data, markup=None, add_text=None):
                                                       f"<b>Город</b> - {str(user_data[3])}\n"
                                                       f"<b>Ваше занятие</b> - {str(user_data[4])}\n"
                                                       f"<b>О себе</b> - {str(user_data[5])}\n\n",
-                             photo=user_data[7], reply_markup=await reciprocity_keyboard())
+                             photo=user_data[7],
+                             reply_markup=await reciprocity_keyboard(user_for_like=user_telegram_id))
 
 
-async def create_questionnaire(state, random_user, chat_id, add_text=None):
-    markup = await questionnaires_keyboard()
-    user_data = await get_data(random_user)
-    await send_questionnaire(chat_id=chat_id, user_data=user_data, markup=markup, add_text=add_text)
-    await state.update_data(data={'questionnaire_owner': random_user})
+async def create_questionnaire(form_owner, chat_id, add_text=None):
+    user_db = await db_commands.select_user(form_owner)
+    markup = await questionnaires_keyboard(target_id=form_owner)
+    user_data = await get_data(form_owner)
+    await send_questionnaire(chat_id=chat_id, user_data=user_data, markup=markup, add_text=add_text, user_db=user_db)
 
 
-async def create_questionnaire_reciprocity(state, random_user, chat_id, add_text=None):
-    user_data = await get_data(random_user)
-    await send_questionnaire(chat_id=chat_id, user_data=user_data, add_text=add_text)
-    await state.update_data(data={'questionnaire_owner': random_user})
+async def create_questionnaire_reciprocity(liker, chat_id, add_text=None, user_db=None):
+    user_data = await get_data(liker)
+    await send_questionnaire(chat_id=chat_id, user_data=user_data, add_text=add_text, user_db=user_db)
 
 
 async def get_meeting_data(telegram_id):
