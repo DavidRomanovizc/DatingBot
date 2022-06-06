64350eddb113337ad4f1eac307f9b17ddd4cb82c
vr4eslav <kldgrcorp@gmail.com>
2022-06-06 12:18:57 +0300

    fix target destinations with callback data and with deleting states p.2

--- handlers/users/view_ques_handler.py
+++ handlers/users/view_ques_handler.py
@@ -74,17 +74,7 @@ async def like_questionnaire(call: CallbackQuery, state: FSMContext, callback_da
 @dp.callback_query_handler(action_reciprocity_keyboard.filter(action=["like_reciprocity", "dislike_reciprocity"]))
 async def like_questionnaire_reciprocity(call: CallbackQuery, state: FSMContext, callback_data: typing.Dict[str, str]):
     user_list = await find_user_gender(call.from_user.id)
-    random_user = None
-    try:
-        random_user = random.choice(user_list)
-    except Exception as err:
-        logger.error(err)
-        await call.message.delete()
-        await call.answer(text="Для вас кончились анкеты!", show_alert=True)
-
     action = callback_data['action']
-
-    await state.update_data(data={'questionnaire_owner': random_user})
     username = call.from_user.username
     if action == "like_reciprocity":
         user_for_like = callback_data["user_for_like"]
