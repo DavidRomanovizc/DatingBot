662c3adcd91543ea0c3b335a458c0a34cdfcd528
DavidRomanovizc <dtdzhalaev@gmail.com>
2022-06-06 14:56:26 +0300

    🐛 Fix bug with tg username

--- handlers/users/start_handler.py
+++ handlers/users/start_handler.py
@@ -13,11 +13,19 @@ from utils.db_api import db_commands
 async def register_user(message: types.Message):
     markup = await start_keyboard()
     try:
-        await db_commands.add_user(name=message.from_user.full_name,
-                                   telegram_id=message.from_user.id,
-                                   username=message.from_user.username)
-        await db_commands.add_meetings_user(telegram_id=message.from_user.id,
-                                            username=message.from_user.username)
+        if message.from_user.username is not None:
+            await db_commands.add_user(name=message.from_user.full_name,
+                                       telegram_id=message.from_user.id,
+                                       username=message.from_user.username)
+            await db_commands.add_meetings_user(telegram_id=message.from_user.id,
+                                                username=message.from_user.username)
+        else:
+            await db_commands.add_user(name=message.from_user.full_name,
+                                       telegram_id=message.from_user.id,
+                                       username="None")
+            await db_commands.add_meetings_user(telegram_id=message.from_user.id,
+                                                username="None")
+
     except:
         pass
     await message.answer(text=_(f"Приветствую вас, {message.from_user.full_name}!!\n\n"
