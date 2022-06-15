3209919091562fb5655518bb5c5e1aa38d3fd958
vr4eslav <kldgrcorp@gmail.com>
2022-06-15 15:18:56 +0300

    new arg for monitoring

--- functions/send_form_func.py
+++ functions/send_form_func.py
@@ -3,7 +3,8 @@ from keyboards.inline.questionnaires_inline import questionnaires_keyboard, back
 from loader import bot
 
 
-async def send_questionnaire(chat_id, user_data, user_db, markup=None, add_text=None):
+async def send_questionnaire(chat_id, user_data, user_db, markup=None, add_text=None,
+                             monitoring=False):
     user_telegram_id = user_db.get("telegram_id")
     if add_text is None:
         await bot.send_photo(chat_id=chat_id, caption=f"{str(user_data[0])}, "
@@ -12,7 +13,8 @@ async def send_questionnaire(chat_id, user_data, user_db, markup=None, add_text=
                                                       f"<b>О себе</b> - {str(user_data[5])}\n\n"
                                                       f"<b>Инстаграм</b> - <code>{str(user_data[8])}</code>\n"
                                                       f"<b>Статус анкеты - {str(user_data[6])}</b>",
-                             photo=user_data[7], reply_markup=await questionnaires_keyboard(target_id=user_telegram_id))
+                             photo=user_data[7], reply_markup=await questionnaires_keyboard(target_id=user_telegram_id,
+                                                                                            monitoring=monitoring))
     elif markup is None:
         await bot.send_photo(chat_id=chat_id, caption=f"{add_text}\n\n"
                                                       f"{str(user_data[0])}, "
