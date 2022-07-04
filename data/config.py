1f95e47335f052e29dec596f23ee8a3b3560dc35
vR4eslav <90927603+vR4eslav@users.noreply.github.com>
2022-07-04 14:43:47 +0300

    Update config.py
    Signed-off-by: vR4eslav <90927603+vR4eslav@users.noreply.github.com>

--- data/config.py
+++ data/config.py
@@ -5,7 +5,7 @@ from environs import Env
 env = Env()
 env.read_env()
 
-support_ids = env.list("SUPPORTS")
+support_ids = list(map(int, env.list("SUPPORTS")))
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = list(map(int, env.list("ADMINS")))
 IP = env.str("IP")
