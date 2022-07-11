9dbc44e0df3dc0c79b4afeb7f1036679a1c3027a
DavidRomanovizc <dtdzhalaev@gmail.com>
2022-07-11 22:29:50 +0300

    Update

--- django_project/telegrambot/telegrambot/urls.py
+++ django_project/telegrambot/telegrambot/urls.py
@@ -12,9 +12,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from django_project.telegrambot.usersmanage import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('export/', views.export_users_csv)
 ]
--- django_project/telegrambot/usersmanage/views.py
+++ django_project/telegrambot/usersmanage/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+import csv
+from django.http import HttpResponse
 
-# Create your views here
+from .models import User
+
+
+def export_users_csv(request):
+    response = HttpResponse(content_type="text/csv")
+
+    writer = csv.writer(response)
+    writer.writerow([
+        "id",
+        "telegram_id",
+        "name",
+        "username",
+        "sex",
+        "age",
+        "city",
+        "need_city",
+        "longitude",
+        "latitude",
+        "verification",
+        "language",
+        "varname",
+        "lifestyle",
+        "is_banned",
+        "photo_id",
+        "commentary",
+        "need_partner_sex",
+        "need_partner_age_min",
+        "need_partner_age_max",
+        "phone_number",
+        "status",
+        "instagram"
+    ])
+    for user in User.objects.all().values_list("id",
+                                               "telegram_id",
+                                               "name",
+                                               "username",
+                                               "sex",
+                                               "age",
+                                               "city",
+                                               "need_city",
+                                               "longitude",
+                                               "latitude",
+                                               "verification",
+                                               "language",
+                                               "varname",
+                                               "lifestyle",
+                                               "is_banned",
+                                               "photo_id",
+                                               "commentary",
+                                               "need_partner_sex",
+                                               "need_partner_age_min",
+                                               "need_partner_age_max",
+                                               "phone_number",
+                                               "status",
+                                               "instagram"):
+        writer.writerow(user)
+
+    response['Content-Disposition'] = 'attachment; filename="users.csv"'
+    return response
