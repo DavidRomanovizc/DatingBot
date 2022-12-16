7bc30d2923f275f447b0aa0893f6d942a8b43307
dromanov <dtdzhalaev@gmail.com>
2022-12-16 19:56:49 +0300

    ✨ Added i18n, qiwi, scheduler

--- loader.py
+++ loader.py
@@ -1,7 +1,13 @@
+from typing import Any
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from glQiwiApi import QiwiWrapper
 
 from data.config import load_config
+from functions.language_ware import setup_middleware
+
 from utils.YandexMap.api import Client
 
 from utils.db_api.postgres import Database
@@ -11,4 +17,10 @@ storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 db = Database()
 client = Client(api_key=load_config().misc.yandex_api_key)
+wallet = QiwiWrapper(api_access_token=load_config().misc.qiwi_key, phone_number=load_config().misc.phone_number,
+                     secret_p2p=load_config().misc.secret_p2p_key)
 # scheduler = await aiojobs.create_scheduler()
+scheduler = AsyncIOScheduler()
+
+i18n = setup_middleware(dp)
+_: Any = i18n.gettext
