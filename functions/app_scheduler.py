89901c18bd1295e18e72cec87930b4003eaab4f8
dromanov <dtdzhalaev@gmail.com>
2022-12-16 20:30:09 +0300

    ✨ Added scheduler

---
+++ functions/app_scheduler.py
@@ -0,0 +1,20 @@
+from aiogram.types import Message
+
+from functions.get_data_func import get_data
+from keyboards.inline.questionnaires_inline import viewing_ques_keyboard
+from loader import bot, _
+
+
+async def send_message_week(message: Message):
+    user = await get_data(message.from_user.id)
+
+    if user[10] == "Мужской":
+        user_gender = "Парней"
+    else:
+        user_gender = "Девушек"
+    text = _("Несколько {user_gender} из города {city}"
+             " хотят познакомиться с тобой прямо сейчас").format(user_gender=user_gender, city=user[12])
+
+    await bot.send_message(chat_id=message.chat.id,
+                           text=text,
+                           reply_markup=await viewing_ques_keyboard())
