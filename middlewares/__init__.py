c1f5d9e3035ec013cec90678899c4ec2bc2f75ef
dromanov <dtdzhalaev@gmail.com>
2022-12-16 20:25:20 +0300

    ♻️ Refactor code

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,12 +1,12 @@
-from loader import dp
+from loader import dp, scheduler
+from .SchedulerWare import SchedulerMiddleware
 from .agent_support import SupportMiddleware
 from .ban_check import BanMiddleware
 from .is_maintenance_check import IsMaintenance
 from .throttling import ThrottlingMiddleware
 
-
 if __name__ == "middlewares":
     dp.middleware.setup(ThrottlingMiddleware())
     dp.middleware.setup(BanMiddleware())
     dp.middleware.setup(SupportMiddleware())
-
+    dp.middleware.setup(SchedulerMiddleware(scheduler))
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,3 @@
 from . import db_api
 from . import misc
 from . import set_bot_commands
-from .notify_admins import on_startup_notify
