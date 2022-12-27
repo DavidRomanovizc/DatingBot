130fe28edfed485bc49d51cdf4ce489d38733d03
dromanov <dtdzhalaev@gmail.com>
2022-12-27 23:26:13 +0300

    ✨ Added new keyboard

--- keyboards/inline/poster_inline.py
+++ keyboards/inline/poster_inline.py
@@ -2,13 +2,31 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from loader import _
 
 
-async def poster_keyboard() -> InlineKeyboardMarkup:
-    markup = InlineKeyboardMarkup()
+async def poster_keyboard(is_admin: bool) -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup(row_width=6)
     create_poster = InlineKeyboardButton(text=_("✍️Создать афишу"), callback_data="create_poster")
-    view_poster = InlineKeyboardButton(text=_("📆 Смотреть афиши"), callback_data="view_poster")
+    view_poster = InlineKeyboardButton(text=_("Смотреть афиши"), callback_data="view_poster")
     my_appointment = InlineKeyboardButton(text=_("📝 Мои записи"), callback_data="my_appointment")
-    markup.row(create_poster, my_appointment)
-    markup.add(view_poster)
+    my_event = InlineKeyboardButton(text=_("🎭 Моё событие"), callback_data="my_event")
+    event_filters = InlineKeyboardButton(text=_("⚙️ Фильтры"), callback_data="event_filters")
+    back = InlineKeyboardButton(text=_("⏪️ Вернуться в меню"), callback_data="start_menu")
+    if is_admin:
+        markup.add(my_event)
+    markup.row(create_poster, event_filters)
+    markup.row(view_poster, my_appointment)
+    markup.add(back)
+    return markup
+
+
+async def event_filters_keyboard() -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup()
+    city_event = InlineKeyboardButton(text=_("Город"), callback_data="city_event")
+    cost_of_event = InlineKeyboardButton(text=_("Стоимость"), callback_data="cost_of_event")
+    event_category = InlineKeyboardButton(text=_("Категории"), callback_data="event_category")
+    back = InlineKeyboardButton(text=_("⏪️ Вернуться в меню"), callback_data="event_menu")
+    markup.add(city_event)
+    markup.row(cost_of_event, event_category)
+    markup.add(back)
     return markup
 
 
