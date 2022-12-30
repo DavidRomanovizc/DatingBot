3b77135f79b017cc99aada995b94aae88b6d38c0
dromanov <dtdzhalaev@gmail.com>
2022-12-30 16:22:25 +0300

    ♻️ Refactor code

--- django_project/telegrambot/telegrambot/wsgi.py
+++ django_project/telegrambot/telegrambot/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for telegrambot project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
