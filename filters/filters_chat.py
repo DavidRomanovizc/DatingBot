836a8eff1c1fca919af0fb3f6585b5e57546820d
dromanov <dtdzhalaev@gmail.com>
2022-12-30 16:37:42 +0300

    🧑‍💻 Added filter for check chat type

--- filters/filters_chat.py
+++ filters/filters_chat.py
@@ -6,3 +6,8 @@ from aiogram.types import Message
 class IsPrivate(BoundFilter):
     async def check(self, message: Message) -> bool:
         return types.ChatType.PRIVATE == message.chat.type
+
+
+class IsGroup(BoundFilter):
+    async def check(self, message: types.Message):
+        return message.chat.type in (types.ChatType.GROUP, types.ChatType.SUPERGROUP)
