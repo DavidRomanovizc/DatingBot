3b4cbdbcf998ce8661d12f3cde56b51236d0b91f
dromanov <dtdzhalaev@gmail.com>
2022-12-31 21:07:54 +0300

    🐛 Fix a bug

--- handlers/groups/event_moderate.py
+++ handlers/groups/event_moderate.py
@@ -3,6 +3,7 @@ import asyncio
 from aiogram.types import CallbackQuery
 
 from data.config import load_config
+from functions.get_data_func import get_data_meetings
 from keyboards.inline.poster_inline import poster_keyboard
 from loader import dp, bot, _
 from utils.db_api import db_commands
@@ -11,13 +12,15 @@ from utils.db_api import db_commands
 @dp.callback_query_handler(lambda call: str(call.message.chat.id) == load_config().tg_bot.moderate_chat)
 async def order_answer(call: CallbackQuery):
     call_data = call.data.split("-")
+    user = await get_data_meetings(call.from_user.id)
+    is_admin = user[10]
 
     if call_data[0] == 'moderate_accept':
         await call.message.delete()
         await call.message.answer(_("Принято!"))
         await db_commands.update_user_meetings_data(telegram_id=call_data[1], verification_status=True)
         await bot.send_message(chat_id=call_data[1], text="Ваше мероприятие прошло модерацию",
-                               reply_markup=await poster_keyboard())
+                               reply_markup=await poster_keyboard(is_admin))
         await asyncio.sleep(30)
 
     elif call_data[0] == 'moderate_decline':
@@ -25,5 +28,5 @@ async def order_answer(call: CallbackQuery):
         await call.message.answer("Отклонено!")
         await db_commands.delete_user_meetings(telegram_id=call_data[1])
         await bot.send_message(chat_id=call_data[1], text="К сожалению ваше мероприятие не прошло модерацию",
-                               reply_markup=await poster_keyboard())
+                               reply_markup=await poster_keyboard(is_admin))
     await db_commands.update_user_meetings_data(telegram_id=call_data[1], moderation_process=True)
