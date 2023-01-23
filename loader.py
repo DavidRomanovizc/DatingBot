d6c4f8ea37b363bc29851650ac6e051fa325c90a
dromanov <dtdzhalaev@gmail.com>
2023-01-23 14:49:30 +0300

    🎨 Added NudeNet

--- loader.py
+++ loader.py
@@ -4,12 +4,11 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from glQiwiApi import QiwiWrapper
+from nudenet import NudeDetector, NudeClassifier
 
 from data.config import load_config
 from functions.main_app.language_ware import setup_middleware
-
 from utils.YandexMap.api import Client
-
 from utils.db_api.postgres import Database
 
 bot = Bot(token=load_config().tg_bot.token, parse_mode=types.ParseMode.HTML)
@@ -21,6 +20,9 @@ wallet = QiwiWrapper(api_access_token=load_config().misc.qiwi_key, phone_number=
                      secret_p2p=load_config().misc.secret_p2p_key)
 # scheduler = await aiojobs.create_scheduler()
 scheduler = AsyncIOScheduler()
+
+detector = NudeDetector()
+classifier = NudeClassifier()
 
 i18n = setup_middleware(dp)
 _: Any = i18n.gettext
