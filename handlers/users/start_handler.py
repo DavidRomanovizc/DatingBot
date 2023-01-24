f112b7f3ef77340b18c2a4d27359cf54ce57c87d
dromanov <dtdzhalaev@gmail.com>
2023-01-24 17:19:30 +0300

    🚑️ Fix bug with username

--- handlers/users/start_handler.py
+++ handlers/users/start_handler.py
@@ -1,70 +1,107 @@
+import asyncio
 import random
 
+import aiogram
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 from aiogram.types import CallbackQuery
-from loguru import logger
+from aiogram.utils.exceptions import BadRequest
 
-from data.config import support_ids
+from data.config import load_config
 from filters import IsPrivate
+from functions.main_app.app_scheduler import send_message_week
+from functions.main_app.auxiliary_tools import registration_menu
+from handlers.users.back_handler import delete_message
+from keyboards.inline.language_inline import language_keyboard
 from keyboards.inline.main_menu_inline import start_keyboard
-from loader import dp, _
+from loader import dp, scheduler, _
 from utils.db_api import db_commands
 
 
 @dp.message_handler(IsPrivate(), CommandStart())
 async def register_user(message: types.Message):
     try:
-        if message.from_user.username is not None:
-            await db_commands.add_user(name=message.from_user.full_name,
-                                       telegram_id=message.from_user.id,
-                                       username=message.from_user.username)
-            await db_commands.add_meetings_user(telegram_id=message.from_user.id,
-                                                username=message.from_user.username)
-        else:
-            await db_commands.add_user(name=message.from_user.full_name,
-                                       telegram_id=message.from_user.id,
-                                       username="None")
-            await db_commands.add_meetings_user(telegram_id=message.from_user.id,
-                                                username="None")
 
+        await db_commands.add_user(name=message.from_user.full_name,
+                                   telegram_id=message.from_user.id,
+                                   username=message.from_user.username)
+        await db_commands.add_meetings_user(telegram_id=message.from_user.id,
+                                            username=message.from_user.username)
+
+        if message.from_user.id in load_config().tg_bot.admin_ids:
+            await db_commands.add_user_to_settings(telegram_id=message.from_user.id)
     except:
         pass
-    support = await db_commands.select_user(telegram_id=support_ids[0])
-    user_db = await db_commands.select_user(telegram_id=message.from_user.id)
-    markup = await start_keyboard(status=user_db["status"])
+    try:
+        support = await db_commands.select_user(telegram_id=load_config().tg_bot.support_ids[0])
+        user_db = await db_commands.select_user(telegram_id=message.from_user.id)
+        markup = await start_keyboard(status=user_db["status"])
+        fullname = message.from_user.full_name
 
-    heart = random.choice(['💙', '💚', '💛', '🧡', '💜', '🖤', '❤', '🤍', '💖', '💝'])
-    await message.answer(text=_(f"Приветствую вас, {message.from_user.full_name}!!\n\n"
-                                f"{heart} <b> QueDateBot </b> - платформа для поиска новых знакомств.\n\n"
-                                f"🪧 Новости о проекте вы можете прочитать в нашем канале - "
-                                f"https://t.me/QueDateGroup \n\n"
-                                f"<b>🤝 Сотрудничество: </b>\n"
-                                f"Если у вас есть предложение о сотрудничестве, пишите агенту поддержки - "
-                                f"@{support['username']}\n\n"
-                                ),
-                         reply_markup=markup)
+        heart = random.choice(['💙', '💚', '💛', '🧡', '💜', '🖤', '❤', '🤍', '💖', '💝'])
+        await message.answer(_("Приветствую вас, {fullname}!!\n\n"
+                               "{heart} <b> QueDateBot </b> - платформа для поиска новых знакомств.\n\n"
+                               "🪧 Новости о проекте вы можете прочитать в нашем канале - "
+                               "https://t.me/QueDateGroup \n\n"
+                               "<b>🤝 Сотрудничество: </b>\n"
+                               "Если у вас есть предложение о сотрудничестве, пишите агенту поддержки - "
+                               "@{supports}\n\n").format(fullname=fullname, heart=heart,
+                                                         supports=support['username']),
+                             reply_markup=markup)
+    except TypeError:
+        await message.answer(_(
+            "У вас не установлен username, пожалуйста,"
+            " зайдите в настройки аккаунта и создайте username.\n"
+            "<b>Без него вы не сможете пользоваться ботом</b>\n\n"
+            '<a href="{url1}">Инструкция №1</a>\n'
+            '<a href="{url2}">Инструкция №2</a>').format(
+            url1="https://www.youtube.com/watch?v=6fC_AUJemSo&ab_channel=TheTechnology",
+            url2="https://www.youtube.com/watch?v=xc9K2NjvfLo&ab_channel=AlexTrack"
+        ))
 
 
 @dp.callback_query_handler(text="start_menu")
 async def start_menu(call: CallbackQuery):
-    user_db = await db_commands.select_user(telegram_id=call.from_user.id)
-    support = await db_commands.select_user(telegram_id=support_ids[0])
-    markup = await start_keyboard(user_db["status"])
-    heart = random.choice(['💙', '💚', '💛', '🧡', '💜', '🖤', '❤', '🤍', '💖', '💝'])
-    await call.message.edit_text(text=_(f"Приветствую вас, {call.from_user.full_name}!!\n\n"
-                                        f"{heart} <b> QueDateBot </b> - платформа для поиска новых знакомств.\n\n"
-                                        f"🪧 Новости о проекте вы можете прочитать в нашем канале - "
-                                        f"https://t.me/QueDateGroup \n\n"
-                                        f"🤝 <b> Сотрудничество: </b>\n"
-                                        f"Если у вас есть предложение о сотрудничестве, пишите агенту поддержки - "
-                                        f"@{support['username']}\n\n"
-                                        ),
-                                 reply_markup=markup)
+    try:
+        await registration_menu(call, scheduler, send_message_week, load_config, random)
+    except TypeError:
+        await call.message.answer(_("Вас нет в базе данной"))
 
 
-@dp.callback_query_handler(text_contains="lang")
+@dp.callback_query_handler(text="language")
+@dp.callback_query_handler(text="language_reg")
+async def choice_language(call: CallbackQuery):
+    if call.data == "language_reg":
+        try:
+            await call.message.edit_text(_("Выберите язык"), reply_markup=await language_keyboard("registration"))
+        except BadRequest:
+            await delete_message(call.message)
+            await call.message.answer(_("Выберите язык"), reply_markup=await language_keyboard("registration"))
+    elif call.data == "language":
+        try:
+            await call.message.edit_text(_("Выберите язык"), reply_markup=await language_keyboard("profile"))
+        except BadRequest:
+            await delete_message(call.message)
+            await call.message.answer(_("Выберите язык"), reply_markup=await language_keyboard("profile"))
+
+
+@dp.callback_query_handler(text="Russian")
+@dp.callback_query_handler(text="Deutsch")
+@dp.callback_query_handler(text="English")
+@dp.callback_query_handler(text="Indonesian")
 async def change_language(call: CallbackQuery):
-    await call.message.edit_reply_markup()
-    lang = call.data[-2:]
-    await call.message.answer(_("Ваш язык был изменен", locale=lang))
+    telegram_id = call.from_user.id
+    try:
+        if call.data == "Russian":
+            await db_commands.update_user_data(telegram_id=telegram_id, language="ru")
+        elif call.data == "Deutsch":
+            await db_commands.update_user_data(telegram_id=telegram_id, language="de")
+        elif call.data == "English":
+            await db_commands.update_user_data(telegram_id=telegram_id, language="en")
+        elif call.data == "Indonesian":
+            await db_commands.update_user_data(telegram_id=telegram_id, language="in")
+        await call.answer(_("Язык был успешно изменен. Введите команду /start"), show_alert=True)
+        await asyncio.sleep(5)
+        await call.message.delete()
+    except aiogram.utils.exceptions.MessageToDeleteNotFound:
+        await call.message.answer(_("Произошла какая-то ошибка. Введите команду /start и попробуйте еще раз"))
--- handlers/users/view_ques_handler.py
+++ handlers/users/view_ques_handler.py
@@ -1,5 +1,6 @@
 import asyncio
 import random
+import secrets
 import typing
 
 from aiogram import types
@@ -7,68 +8,68 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import CallbackQuery
 from loguru import logger
 
-from functions.create_forms_funcs import create_questionnaire, create_questionnaire_reciprocity
-from functions.get_data_func import get_data
-from functions.get_next_user_func import get_next_user
+from functions.dating.create_forms_funcs import create_questionnaire, create_questionnaire_reciprocity, rand_user_list
+from functions.dating.get_next_user_func import get_next_user
+from functions.main_app.get_data_func import get_data
 from handlers.users.back_handler import delete_message
 from keyboards.inline.main_menu_inline import start_keyboard
 from keyboards.inline.questionnaires_inline import action_keyboard, action_reciprocity_keyboard
-from keyboards.inline.registration_inline import registration_keyboard
-from loader import dp
+from loader import dp, _, bot
 from utils.db_api import db_commands
 
 
-@dp.callback_query_handler(text='find_ancets')
+@dp.callback_query_handler(text='find_ques')
 async def start_finding(call: CallbackQuery, state: FSMContext):
     try:
         telegram_id = call.from_user.id
-        user_data = await get_data(telegram_id)
-        user_list = await get_next_user(telegram_id)
-        user_status = user_data[9]
-        if user_status:
-            random_user = random.choice(user_list)
-            await delete_message(call.message)
-            await create_questionnaire(form_owner=random_user, chat_id=telegram_id)
-            await state.set_state('finding')
-        else:
-            await call.message.edit_text("Вам необходимо зарегистрироваться, нажмите на кнопку ниже",
-                                         reply_markup=await registration_keyboard())
+        user_list = await get_next_user(telegram_id, call)
+        random_user = random.choice(user_list)
+        await create_questionnaire(form_owner=random_user, chat_id=telegram_id)
+        await state.set_state("finding")
     except IndexError:
-        await call.answer("На данный момент у нас нет подходящих анкет для вас")
+        await call.answer(_("На данный момент у нас нет подходящих анкет для вас"), show_alert=True)
 
 
 @dp.callback_query_handler(action_keyboard.filter(action=["like", "dislike", "stopped"]),
                            state='finding')
 async def like_questionnaire(call: CallbackQuery, state: FSMContext, callback_data: typing.Dict[str, str]):
-    user_list = await get_next_user(call.from_user.id)
-    random_user = random.choice(user_list)
     action = callback_data['action']
     user_db = await db_commands.select_user(telegram_id=call.from_user.id)
     username = call.from_user.username
-    if action == "like":
-        try:
-            target_id = callback_data["target_id"]
-            await create_questionnaire(form_owner=call.from_user.id, chat_id=target_id,
-                                       add_text=f'Вами заинтересовался пользователь '
-                                                f'<a href="https://t.me/{username}">{username}</a>')
-            await create_questionnaire(form_owner=random_user, chat_id=call.from_user.id)
+    varname = await get_data(call.from_user.id)
 
-            await state.reset_data()
-        except Exception as err:
-            logger.error(err)
-            await create_questionnaire(form_owner=random_user, chat_id=call.from_user.id)
+    if action == "like":
+        if username is not None:
+            text = _('Вами заинтересовался пользователь '
+                     '<a href="https://t.me/{username}">{varname_0}</a>').format(username=username,
+                                                                                 varname_0=varname[0])
+
+        else:
+            text = _('Вами заинтересовался пользователь '
+                     '<a href="tg://openmessage?user_id={chat_id}">{varname_0}</a>').format(chat_id=call.from_user.id,
+                                                                                            varname_0=varname[0])
+        target_id = callback_data["target_id"]
+        await create_questionnaire(form_owner=call.from_user.id, chat_id=target_id,
+                                   add_text=text)
+
+        await call.message.delete()
+        await create_questionnaire(form_owner=(await rand_user_list(call))[3], chat_id=call.from_user.id)
+
+        await state.reset_data()
     elif action == "dislike":
         try:
-            await create_questionnaire(form_owner=random_user, chat_id=call.from_user.id)
+            await call.message.delete()
+            await create_questionnaire(form_owner=(await rand_user_list(call))[3], chat_id=call.from_user.id)
             await state.reset_data()
         except Exception as err:
             logger.error(err)
-            await create_questionnaire(form_owner=random_user, chat_id=call.from_user.id)
+            await create_questionnaire(form_owner=(await rand_user_list(call))[2], chat_id=call.from_user.id)
     elif action == "stopped":
         markup = await start_keyboard(user_db["status"])
         await call.message.delete()
-        await call.message.answer(f"Рад был помочь, {call.from_user.full_name}!\n"
-                                  f"Надеюсь, ты нашел кого-то благодаря мне", reply_markup=markup)
+        text = _("Рад был помочь, {fullname}!\n"
+                 "Надеюсь, ты нашел кого-то благодаря мне").format(fullname=call.from_user.full_name)
+        await call.message.answer(text, reply_markup=markup)
         await state.reset_state()
 
 
@@ -76,44 +77,49 @@ async def like_questionnaire(call: CallbackQuery, state: FSMContext, callback_da
 async def like_questionnaire_reciprocity(call: CallbackQuery, state: FSMContext, callback_data: typing.Dict[str, str]):
     action = callback_data['action']
     username = call.from_user.username
-    user_db = await db_commands.select_user(telegram_id=call.from_user.id)
+    varname = await get_data(call.from_user.id)
     if action == "like_reciprocity":
         user_for_like = callback_data["user_for_like"]
         user_db = await db_commands.select_user(telegram_id=call.from_user.id)
         await asyncio.sleep(1)
-        await call.message.delete()
-        await call.message.answer("Ваша анкета отправлена другому пользователю",
+
+        await bot.edit_message_reply_markup(chat_id=call.from_user.id,
+                                            message_id=call.message.message_id,
+                                            reply_markup=None)
+        await call.message.answer(_("Ваша анкета отправлена другому пользователю"),
                                   reply_markup=await start_keyboard(user_db["status"]))
+
         await asyncio.sleep(5)
         await create_questionnaire_reciprocity(liker=call.from_user.id, chat_id=user_for_like,
                                                add_text=f'Вам ответили взаимностью, пользователь - '
-                                                        f'<a href="https://t.me/{username}">{username}</a>',
+                                                        f'<a href="https://t.me/{username}">{varname[0]}</a>',
                                                user_db=user_db)
         await state.reset_state()
     elif action == "dislike_reciprocity":
         await asyncio.sleep(1)
         await delete_message(call.message)
-        await call.message.answer("Меню: ", reply_markup=await start_keyboard(user_db["status"]))
         await state.reset_state()
+        user_db = await db_commands.select_user(telegram_id=call.from_user.id)
+        await call.message.answer(_("Меню: "), reply_markup=await start_keyboard(user_db["status"]))
     await state.reset_state()
 
 
-@dp.callback_query_handler(text="go_back_to_viewing_ques", state='finding')
+@dp.callback_query_handler(state="*", text="go_back_to_viewing_ques")
 async def like_questionnaire(call: CallbackQuery, state: FSMContext):
-    user_list = await get_next_user(call.from_user.id)
-    random_user = random.choice(user_list)
-
+    await call.message.delete()
+    user_list = await get_next_user(call.from_user.id, call)
+    random_user = secrets.choice(user_list)
     try:
         await create_questionnaire(form_owner=random_user, chat_id=call.from_user.id)
 
         await state.reset_data()
-    except Exception as err:
-        logger.error(err)
-        await create_questionnaire(form_owner=random_user, chat_id=call.from_user.id)
+    except IndexError:
+        await call.answer(_("На данный момент у нас нет подходящих анкет для вас"))
+        await state.reset_data()
 
 
 @dp.message_handler(state='finding')
 async def echo_message_finding(message: types.Message, state: FSMContext):
     user_db = await db_commands.select_user(telegram_id=message.from_user.id)
-    await message.answer("Меню: ", reply_markup=await start_keyboard(user_db["status"]))
-    await state.reset_state(with_data=True)
+    await message.answer(_("Меню: "), reply_markup=await start_keyboard(user_db["status"]))
+    await state.reset_state()
