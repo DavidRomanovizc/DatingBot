ae6225a54c210693c3b487d9187351e51b23e85b
dromanov <dtdzhalaev@gmail.com>
2023-04-15 23:12:01 +0300

    🔊 Update logs

--- utils/misc/logging.py
+++ utils/misc/logging.py
@@ -1,4 +1,16 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
-                    level=logging.INFO)
+handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+
+handler.setFormatter(formatter)
+
+handler.setLevel(logging.ERROR)
+
+logger = logging.getLogger('myapp')
+
+logger.setLevel(logging.INFO)
+
+logger.addHandler(handler)
