925fdc9b0f20a598b4def00a03f382c57da1f916
dromanov <dtdzhalaev@gmail.com>
2023-04-15 23:05:50 +0300

    ✅ added test for group start handler

---
+++ test/test_handlers/test_group_start.py
@@ -0,0 +1,14 @@
+from unittest.mock import AsyncMock
+
+import pytest
+
+from handlers.groups.start import start_group_handler
+from loader import _
+
+
+@pytest.mark.asyncio
+async def test_start_group_handler():
+    text_mock = _("<b>Привет, я бот, проекта Que Group, для верификации анкет для знакомств</b>\n\n")
+    message_mock = AsyncMock(text=text_mock)
+    await start_group_handler(message=message_mock)
+    message_mock.answer.assert_called_with(text_mock)
