0498846b2f9f8d69561d10d7ab91a27dc2776463
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2023-06-25 16:05:20 +0300

    ✨ Add SettingModel into admin

--- django_project/telegrambot/usersmanage/admin.py
+++ django_project/telegrambot/usersmanage/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, UserMeetings
+from .models import User, UserMeetings, SettingModel
 
 
 @admin.register(User)
@@ -11,3 +11,7 @@ class UserAdmin(admin.ModelAdmin):
 @admin.register(UserMeetings)
 class UserMeetingsAdmin(admin.ModelAdmin):
     list_display = ("id", "telegram_id", "username", "created_at")
+
+@admin.register(SettingModel)
+class UserSettingsAdmin(admin.ModelAdmin):
+    list_display = ("telegram_id", "technical_works")
