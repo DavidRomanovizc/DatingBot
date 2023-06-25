3e3bca7fe229a18b196f187f0fc5de8866c9c5eb
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2023-06-25 15:54:41 +0300

    🔥 Remove unused code

--- keyboards/inline/registration_inline.py
+++ keyboards/inline/registration_inline.py
@@ -3,13 +3,6 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from loader import _
 
 
-async def registration_keyboard() -> InlineKeyboardMarkup:
-    markup = InlineKeyboardMarkup(row_width=2)
-    registration = InlineKeyboardButton(text=_("➕ Регистрация"), callback_data="registration")
-    markup.add(registration)
-    return markup
-
-
 async def second_registration_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(row_width=1)
     questionnaire = InlineKeyboardButton(text=_("🖌️ Пройти опрос в боте"), callback_data="survey")
@@ -22,12 +15,4 @@ async def confirm_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(row_width=2)
     yes_all_good = InlineKeyboardButton(text=_("✅ Да все хорошо!"), callback_data="yes_all_good")
     markup.add(yes_all_good)
-    return markup
-
-
-async def about_yourself_keyboard() -> InlineKeyboardMarkup:
-    markup = InlineKeyboardMarkup()
-    text = InlineKeyboardButton(text=_("💬 Текстом"), callback_data="send_text")
-    voice = InlineKeyboardButton(text=_("🎤 Голосовым"), callback_data="send_voice")
-    markup.add(text, voice)
     return markup
