f7541581a542241fc0f676587e6fb9c2d47ebb68
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2023-08-14 16:20:30 +0300

    ✨ Payment system

---
+++ utils/yoomoney/types/__init__.py
@@ -0,0 +1,4 @@
+from .account_info import AccountInfo
+from .operation_details import OperationDetails
+from .operation_history import Operation
+from .payment import PaymentForm, PaymentSource
---
+++ utils/yoomoney/types/operation_details.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from typing import Literal
+
+from pydantic import BaseModel, Field
+
+
+class OperationDetails(BaseModel):
+    """
+    Детальная информация об операции из истории
+    https://yoomoney.ru/docs/wallet/user-account/operation-details
+    """
+    error: str | None
+    operation_id: str
+    status: str
+    pattern_id: str | None
+    direction: Literal["in"] | Literal["out"]
+    amount: int
+    amount_due: int | None
+    fee: int | None
+    operation_datetime: datetime = Field(alias="datetime")
+    title: str
+    sender: int | None
+    recipient: str | None
+    recipient_type: str | None
+    message: str | None
+    comment: str | None
+    label: str | None
+    details: str | None
+    operation_type: str = Field(alias="type")
