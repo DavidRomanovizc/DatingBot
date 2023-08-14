fb3a22b9cb4fa67e69147b243f252592c2cc9c77
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2023-08-14 18:47:21 +0300

    🐛 Fix bug with unban

--- handlers/users/buy_unban.py
+++ handlers/users/buy_unban.py
@@ -1,8 +1,11 @@
+import uuid
+
 from aiogram.dispatcher import FSMContext
 from aiogram.types import CallbackQuery
 
 from data.config import load_config
 from keyboards.inline.admin_inline import unban_user_keyboard
+from keyboards.inline.main_menu_inline import start_keyboard
 from keyboards.inline.payments_inline import payments_keyboard
 from loader import dp, bot, _, wallet
 from utils.db_api import db_commands
@@ -10,36 +13,58 @@ from utils.yoomoney.types import PaymentSource
 
 
 @dp.callback_query_handler(text="unban")
-async def get_payment(call: CallbackQuery) -> None:
-    await call.answer(cache_time=60)
+async def get_payment(call: CallbackQuery, state: FSMContext) -> None:
     payment_form = await wallet.create_payment_form(
-        amount_rub=1,
-        unique_label="Dating",
+        amount_rub=2,
+        unique_label=uuid.uuid4().hex,
         payment_source=PaymentSource.YOOMONEY_WALLET,
         success_redirect_url=load_config().misc.redirect_url,
     )
-    payment_is_completed: bool = await wallet.check_payment_on_successful(payment_form.payment_label)
-    print(payment_is_completed)
+
     await call.message.edit_text(
         text=_(
             "<b>💳 Стоимость разбана - 600</b>\n"
             "├Чтобы проверить актуальность цен, нажмите на кнопку \n"
-            "├🔄 Проверить цены\n"
             "├Оплата обычно приходить в течение 1-3 минут\n\n"
-            "<b>Форма оплачена {status}</b>".format(
-                status='Да' if payment_is_completed else 'Нет'
-            )
         ),
-        reply_markup=await payments_keyboard("unban", url=payment_form.link_for_customer)
+        reply_markup=await payments_keyboard(url=payment_form.link_for_customer)
     )
+
+    await state.set_state("payment")
+    await state.update_data(
+        {
+            "label": payment_form.payment_label,
+            "form": payment_form.link_for_customer
+        }
+    )
+
+
+@dp.callback_query_handler(text="check_payment", state="payment")
+async def check_payment(call: CallbackQuery, state: FSMContext) -> None:
+    data = await state.get_data()
+    payment_is_completed: bool = await wallet.check_payment_on_successful(label=data.get("label"))
+    markup = await start_keyboard(obj=call)
     if payment_is_completed:
+        await call.message.edit_text(
+            text=_(
+                "Поздравляем! Вы были разрабанены"
+            ),
+            reply_markup=markup
+        )
         await db_commands.update_user_data(telegram_id=call.from_user.id, is_banned=False)
+        await state.reset_state()
+    else:
+        await call.message.edit_text(
+            text=_(
+                "Оплата не прошла! Подождите минут 10, а затем еще раз попробуйте нажать кнопку ниже"
+            ),
+            reply_markup=await payments_keyboard(url=data.get("form"))
+        )
 
 
-@dp.callback_query_handler(state='payment', text='cancel_payment')
+@dp.callback_query_handler(state='payment', text='unban_menu')
 async def cancel_payment(call: CallbackQuery, state: FSMContext) -> None:
-    await call.answer(cache_time=60)
-    await call.message.edit_text(_("Вы забанены!"), reply_markup=await unban_user_keyboard())
+    await call.message.answer(_("Вы забанены!"), reply_markup=await unban_user_keyboard())
     await state.reset_state()
 
 
--- keyboards/inline/payments_inline.py
+++ keyboards/inline/payments_inline.py
@@ -4,33 +4,9 @@ from yarl import URL
 from loader import _
 
 
-async def payments_keyboard(menu: str, url: str | URL = None) -> InlineKeyboardMarkup:
-    markup = InlineKeyboardMarkup(row_width=4)
+async def payments_keyboard(url: str | URL = None) -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup(row_width=1)
     pay_qiwi = InlineKeyboardButton(text=_("💳 ЮMoney"), url=url)
-    check_prices = InlineKeyboardButton(text=_("🔄 Проверить цены"), callback_data="check_price")
+    check_prices = InlineKeyboardButton(text=_("🔄 Проверить оплату"), callback_data="check_payment")
     markup.add(pay_qiwi, check_prices)
-    if menu == "unban":
-        back = InlineKeyboardButton(text=_("⏪️ Вернуться в меню"), callback_data="unban_menu")
-        markup.add(back)
-    else:
-        back = InlineKeyboardButton(text=_("⏪️ Вернуться в меню"), callback_data="start_menu")
-        markup.add(back)
-    return markup
-
-
-async def making_payment(bill) -> InlineKeyboardMarkup:
-    markup = InlineKeyboardMarkup()
-    settle_bill = InlineKeyboardButton(text=_("Оплатить"), url=bill.pay_url)
-    check_payment = InlineKeyboardButton(text=_("Проверить оплату"), callback_data='check_payment')
-    cancel = InlineKeyboardButton(text=_("Отмена"), callback_data='cancel_payment')
-    markup.add(settle_bill)
-    markup.add(check_payment)
-    markup.add(cancel)
-    return markup
-
-
-async def check_payment_keyboard() -> InlineKeyboardMarkup:
-    markup = InlineKeyboardMarkup()
-    check_payment = InlineKeyboardButton(text=_("Проверить оплату"), callback_data='check_payment')
-    markup.add(check_payment)
     return markup
