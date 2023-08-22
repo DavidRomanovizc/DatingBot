a2a184a80bed4335e4e4decc0a9f2b067dbae014
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2023-08-22 16:04:18 +0300

    🎨 Rewrite Location to Strategy pattern

--- functions/main_app/determin_location.py
+++ functions/main_app/determin_location.py
@@ -1,66 +1,82 @@
+from abc import ABC, abstractmethod
+from typing import Type, Union
+
 from aiogram.types import Message
 
 from keyboards.inline.registration_inline import confirm_keyboard
-from loader import _
+from loader import _, logger
 from loader import client
+from utils.YandexMap.exceptions import NothingFound
 from utils.db_api import db_commands
 from utils.misc.AsyncObj import AsyncObj
+
+
+class UserDataUpdateStrategy(ABC):
+    @abstractmethod
+    async def update_user_data(self: Union["Location", Message]):
+        pass
 
 
 class Location(AsyncObj):
     """
     Класс для определения и сохранения локации пользователя
-    во время регистрации анкеты, изменения данных,
-    регистрации мероприятия.
+    в различных сценариях.
     """
 
-    async def __ainit__(self, message: Message) -> None:
+    async def __ainit__(self, message: Message, strategy: Type[UserDataUpdateStrategy]) -> None:
         self.markup = await confirm_keyboard()
         self.x, self.y = await client.coordinates(message.text)
         self.city = await client.address(f"{self.x}", f"{self.y}")
         self.text = _('Я нашел такой адрес:\n'
                       '<b>{city}</b>\n'
-                      'Если все правильно то подтвердите').format(city=self.city)
+                      'Если все правильно, то подтвердите').format(city=self.city)
+        self.strategy = strategy
+        self.message = message
 
-    async def det_loc_in_registration(self, message: Message) -> None:
-        await message.answer(
-            self.text,
-            reply_markup=self.markup
-        )
+    async def det_loc(self) -> None:
+        if self.city is None:
+            raise NothingFound
+        else:
+            await self.message.answer(
+                self.text,
+                reply_markup=self.markup
+            )
+            try:
+                await self.strategy.update_user_data(self)
+            except TypeError:
+                logger.info("Error in det_loc")
+
+
+class RegistrationStrategy(UserDataUpdateStrategy):
+    async def update_user_data(self: Union[Location, Message]):
         await db_commands.update_user_data(
-            telegram_id=message.from_user.id,
+            telegram_id=self.from_user.id,
             city=self.city,
             need_city=self.city,
             longitude=self.x,
             latitude=self.y
         )
 
-    async def det_loc_in_filters(self, message: Message) -> None:
-        await message.answer(
-            self.text,
-            reply_markup=self.markup
-        )
+
+class FiltersStrategy(UserDataUpdateStrategy):
+    async def update_user_data(self: Union[Location, Message]):
         await db_commands.update_user_data(
-            telegram_id=message.from_user.id,
+            telegram_id=self.from_user.id,
             need_city=self.city
         )
 
-    async def det_loc_in_event(self, message: Message) -> None:
-        await message.answer(
-            self.text,
-            reply_markup=self.markup
-        )
+
+class EventStrategy(UserDataUpdateStrategy):
+    async def update_user_data(self: Union[Location, Message]):
         await db_commands.update_user_meetings_data(
-            telegram_id=message.from_user.id,
+            telegram_id=self.from_user.id,
             venue=self.city
         )
 
-    async def det_loc_in_filters_event(self, message: Message) -> None:
-        await message.answer(
-            self.text,
-            reply_markup=self.markup
-        )
+
+class EventFiltersStrategy(UserDataUpdateStrategy):
+    async def update_user_data(self: Union[Location, Message]):
         await db_commands.update_user_meetings_data(
-            telegram_id=message.from_user.id,
+            telegram_id=self.from_user.id,
             need_location=self.city
         )
