4cdd5cebe1c6138452e818df7307637ad10bcae1
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2023-08-22 14:53:08 +0300

    ✨ Add scheduler for reset limit

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import filters
 from django_project.telegrambot.telegrambot import settings
 # noinspection PyUnresolvedReferences
 from loader import dp, db, scheduler
+from utils.db_api.db_commands import reset_view_limit
 from utils.logger import setup_logger
 from utils.notify_admins import AdminNotification
 from utils.set_bot_commands import set_default_commands
@@ -18,7 +19,13 @@ from utils.set_bot_commands import set_default_commands
 
 async def on_startup(dispatcher) -> None:
     await set_default_commands(dispatcher)
-
+    scheduler.add_job(
+        func=reset_view_limit,
+        trigger="cron",
+        hour=0,
+        id="reset_view_limit",
+        replace_existing=True
+    )
     await AdminNotification.send(dispatcher)
 
 
